231218a87a5e17c8fec92e37c6c453f9e41f13de
Benedict <kontakt@folivo.net>
2023-07-11 10:52:38 +0200

    upgraded dependencies

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -3,22 +3,22 @@ import org.gradle.jvm.toolchain.JavaLanguageVersion
 
 object Versions {
     val kotlinJvmTarget = JavaVersion.VERSION_11
-    const val trixnity = "3.7.0-beta2" // https://gitlab.com/trixnity/trixnity/-/releases
-    const val kotlin = "1.8.10" // https://kotlinlang.org/
-    const val kotlinxCoroutines = "1.7.1" // https://github.com/Kotlin/kotlinx.coroutines/releases
-    const val kotlinxSerialization = "1.5.0" // https://github.com/Kotlin/kotlinx.serialization/releases
+    const val trixnity = "3.7.0-beta3" // https://gitlab.com/trixnity/trixnity/-/releases
+    const val kotlin = "1.8.20" // https://kotlinlang.org/
+    const val kotlinxCoroutines = "1.7.2" // https://github.com/Kotlin/kotlinx.coroutines/releases
+    const val kotlinxSerialization = "1.5.1" // https://github.com/Kotlin/kotlinx.serialization/releases
     const val decompose = "1.0.0-compose-experimental" // https://github.com/arkivanov/Decompose/releases
     const val essenty = "1.0.0" // https://github.com/arkivanov/Essenty/releases
     const val kotlinxDatetime = "0.4.0" // https://github.com/Kotlin/kotlinx-datetime/releases
     const val koin = "3.4.2" // https://github.com/InsertKoinIO/koin/tags
-    const val ktor = "2.3.1" // https://github.com/ktorio/ktor/releases
-    const val mocKMP = "1.13.0" // https://github.com/Kodein-Framework/MocKMP/releases
+    const val ktor = "2.3.2" // https://github.com/ktorio/ktor/releases
+    const val mocKMP = "1.14.0" // https://github.com/Kodein-Framework/MocKMP/releases
     const val kotest = "5.6.2" // https://github.com/kotest/kotest/releases
     const val jna = "5.13.0" // https://github.com/java-native-access/jna/tags
-    const val okio = "3.3.0" // https://square.github.io/okio/changelog
-    const val uuid = "0.7.0" // https://github.com/benasher44/uuid/releases
+    const val okio = "3.4.0" // https://square.github.io/okio/changelog
+    const val uuid = "0.7.1" // https://github.com/benasher44/uuid/releases
     const val multiplatformSettings = "1.0.0" // https://github.com/russhwolf/multiplatform-settings/releases
-    const val korge = "4.0.7" // https://github.com/korlibs/korge/releases
+    const val korge = "4.0.8" // https://github.com/korlibs/korge/releases
 
     // integration tests
     const val testContainers = "1.18.3" // https://github.com/testcontainers/testcontainers-java/releases
