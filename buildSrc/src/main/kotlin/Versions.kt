0d71e4b869562182d533490eb5317d29ee97742b
michael.thiele <thiele.michael2@gmail.com>
2023-08-11 11:32:56 +0200

    bump version to 1.0.1

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -2,7 +2,7 @@ import org.gradle.api.JavaVersion
 import org.gradle.jvm.toolchain.JavaLanguageVersion
 
 object Versions {
-    const val trixnityMessenger = "1.0.0"
+    const val trixnityMessenger = "1.0.1"
 
     val kotlinJvmTarget = JavaVersion.VERSION_11
     const val trixnity = "3.10.0" // https://gitlab.com/trixnity/trixnity/-/releases
