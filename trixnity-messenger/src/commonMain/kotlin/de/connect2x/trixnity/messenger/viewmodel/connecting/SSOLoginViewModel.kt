4213d1b33252aeef6255b85ace99d4499222400d
benkuly <kontakt@folivo.net>
2023-08-29 14:14:19 +0200

    println for debugging

--- trixnity-messenger/src/commonMain/kotlin/de/connect2x/trixnity/messenger/viewmodel/connecting/SSOLoginViewModel.kt
+++ trixnity-messenger/src/commonMain/kotlin/de/connect2x/trixnity/messenger/viewmodel/connecting/SSOLoginViewModel.kt
@@ -79,6 +79,10 @@ open class SSOLoginViewModelImpl(
     init {
         coroutineScope.launch {
             urlHandler.filter {
+                println(it.encodedPath)
+                println(it.host)
+                println(it.port)
+                println(it.parameters)
                 it.encodedPath.removeSuffix("/") == messengerSettings.ssoRedirectUrl
             }.collect {
                 val loginToken = it.parameters["loginToken"]
