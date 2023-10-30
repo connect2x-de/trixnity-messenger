37048e549253d3b1e26d1cda0ae1333b5c22a8a5
benkuly <kontakt@folivo.net>
2023-10-30 12:10:49 +0100

    use EncryptedSharedPreferences for secrets in android

--- trixnity-messenger/src/androidMain/kotlin/de/connect2x/trixnity/messenger/util/Secrets.kt
+++ trixnity-messenger/src/androidMain/kotlin/de/connect2x/trixnity/messenger/util/Secrets.kt
@@ -1,62 +1,42 @@
 package de.connect2x.trixnity.messenger.util
 
-import androidx.security.crypto.EncryptedFile
+import android.content.SharedPreferences
+import androidx.security.crypto.EncryptedSharedPreferences
 import androidx.security.crypto.MasterKey
 import de.connect2x.trixnity.messenger.getContext
 import io.github.oshai.kotlinlogging.KotlinLogging
-import java.io.File
-import java.io.FileInputStream
-import java.io.FileOutputStream
-import java.io.IOException
 
 private val log = KotlinLogging.logger { }
 
+private fun getEncryptedSharedPreferences(): SharedPreferences {
+    val masterKeyAlias = MasterKey.Builder(getContext()).setKeyScheme(MasterKey.KeyScheme.AES256_GCM).build()
+
+    return EncryptedSharedPreferences.create(
+        getContext(),
+        "encrypted_secrets",
+        masterKeyAlias,
+        EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+        EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+    )
+}
+
 actual suspend fun getSecret(id: String): String? {
     try {
-        val masterKey = MasterKey.Builder(getContext()).setKeyScheme(MasterKey.KeyScheme.AES256_GCM).build()
-
-        val file = File(getContext().filesDir, id)
-        val encryptedFile = EncryptedFile.Builder(
-            getContext(),
-            file,
-            masterKey,
-            EncryptedFile.FileEncryptionScheme.AES256_GCM_HKDF_4KB
-        ).build()
-
-        // write to the encrypted file
-        val encryptedInputStream: FileInputStream = encryptedFile.openFileInput()
-        log.info { "read secret ('$id') from encrypted file" }
-        val password = encryptedInputStream.readBytes().decodeToString()
-        encryptedInputStream.close()
-        return password
-    } catch (exc: IOException) {
-        log.error(exc) {}
-        log.warn { "Cannot find secret ('$id') file." }
+        val encryptedSharedPreferences = getEncryptedSharedPreferences()
+        return encryptedSharedPreferences.getString(id, null)
     } catch (exc: Exception) {
-        log.error(exc) { "Cannot read secret ('$id') file." }
+        log.error(exc) { "Cannot read of find secret ('$id')." }
     }
-
     return null
 }
 
 actual suspend fun setSecret(id: String, secret: String) {
     try {
-        val masterKey = MasterKey.Builder(getContext()).setKeyScheme(MasterKey.KeyScheme.AES256_GCM).build()
-
-        val file = File(getContext().filesDir, id)
-        val encryptedFile = EncryptedFile.Builder(
-            getContext(),
-            file,
-            masterKey,
-            EncryptedFile.FileEncryptionScheme.AES256_GCM_HKDF_4KB
-        ).build()
-
-        // write to the encrypted file
-        val encryptedOutputStream: FileOutputStream = encryptedFile.openFileOutput()
-        log.info { "write secret ('$id') to encrypted file" }
-        encryptedOutputStream.write(secret.toByteArray())
-        encryptedOutputStream.close()
+        val encryptedSharedPreferences = getEncryptedSharedPreferences()
+        encryptedSharedPreferences.edit().apply {
+            putString(id, secret)
+        }.apply()
     } catch (exc: Exception) {
-        log.error(exc) { "Cannot write secret ('$id') to file." }
+        log.error(exc) { "Cannot write secret ('$id')." }
     }
 }
