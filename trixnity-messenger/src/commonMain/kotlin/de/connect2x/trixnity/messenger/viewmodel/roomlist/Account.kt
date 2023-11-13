b8f344b5e6be880a07c1b9a0f1e432dcb7f9e99c
Janne Mareike Koschinski <janne-mareike.koschinski@nordeck.net>
2023-11-13 12:28:39 +0100

    expose UserId in AccountViewModel

--- trixnity-messenger/src/commonMain/kotlin/de/connect2x/trixnity/messenger/viewmodel/roomlist/Account.kt
+++ trixnity-messenger/src/commonMain/kotlin/de/connect2x/trixnity/messenger/viewmodel/roomlist/Account.kt
@@ -4,6 +4,7 @@ import kotlinx.coroutines.flow.StateFlow
 
 data class Account(
     val accountName: String, // primary key
+    val userId: UserId,
     val displayName: StateFlow<String>,
     val initials: StateFlow<String>,
     val avatar: StateFlow<ByteArray?>
