aec0579ebc923113fe283edeb468ffaec84610f6
benkuly <kontakt@folivo.net>
2024-02-02 08:16:12 +0100

    upgrade dependencies

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -5,17 +5,17 @@ object Versions {
     const val trixnityMessenger = "2.0.0"
 
     val kotlinJvmTarget = JavaVersion.VERSION_11
-    const val trixnity = "4.2.0" // https://gitlab.com/trixnity/trixnity/-/releases
+    const val trixnity = "4.2.1" // https://gitlab.com/trixnity/trixnity/-/releases
     const val kotlin = "1.9.22" // https://kotlinlang.org/
     const val ksp = "1.9.22-1.0.16" // https://github.com/google/ksp/releases
-    const val kotlinWrappers = "1.0.0-pre.674" // https://github.com/JetBrains/kotlin-wrappers/releases
+    const val kotlinWrappers = "1.0.0-pre.693" // https://github.com/JetBrains/kotlin-wrappers/releases
     const val kotlinxCoroutines = "1.7.3" // https://github.com/Kotlin/kotlinx.coroutines/releases
     const val kotlinxSerialization = "1.6.2" // https://github.com/Kotlin/kotlinx.serialization/releases
     const val decompose = "2.2.2" // https://github.com/arkivanov/Decompose/releases
     const val kotlinxDatetime = "0.5.0" // https://github.com/Kotlin/kotlinx-datetime/releases
     const val koin = "3.5.3" // https://github.com/InsertKoinIO/koin/tags
-    const val ktor = "2.3.7" // https://github.com/ktorio/ktor/releases
-    const val mocKmp = "1.16.0" // https://github.com/Kodein-Framework/MocKMP/releases
+    const val ktor = "2.3.8" // https://github.com/ktorio/ktor/releases
+    const val mocKmp = "1.17.0" // https://github.com/Kodein-Framework/MocKMP/releases
     const val kotest = "5.8.0" // https://github.com/kotest/kotest/releases
     const val jna = "5.14.0" // https://github.com/java-native-access/jna/tags
     const val okio = "3.7.0" // https://square.github.io/okio/changelog
@@ -27,10 +27,9 @@ object Versions {
     const val kmmBridge = "0.5.1" // https://github.com/touchlab/KMMBridge/releases
 
     // integration tests
-    const val testContainers = "1.19.3" // https://github.com/testcontainers/testcontainers-java/releases
-    const val h2 = "2.2.224" // https://github.com/h2database/h2database/releases
+    const val testContainers = "1.19.4" // https://github.com/testcontainers/testcontainers-java/releases
 
-    const val kotlinLogging = "6.0.1" // https://github.com/MicroUtils/kotlin-logging/releases
+    const val kotlinLogging = "6.0.3" // https://github.com/MicroUtils/kotlin-logging/releases
     const val logback = "1.4.14" // https://github.com/qos-ch/logback/tags
 
     const val crypto = "1.1.0-alpha06" // https://developer.android.com/jetpack/androidx/releases/security
