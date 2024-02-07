1b79d5ca13a826a7e982cd5bd853b202a8b56a21
Adam Brangenberg <adambrangenberg@proton.me>
2024-02-07 22:26:35 +0100

    Adding field userId to UserInfoElement

--- trixnity-messenger/src/commonMain/kotlin/de/connect2x/trixnity/messenger/viewmodel/UserElement.kt
+++ trixnity-messenger/src/commonMain/kotlin/de/connect2x/trixnity/messenger/viewmodel/UserElement.kt
@@ -1,19 +1,12 @@
 package de.connect2x.trixnity.messenger.viewmodel
 
-import de.connect2x.trixnity.messenger.viewmodel.util.Initials
-import de.connect2x.trixnity.messenger.viewmodel.util.avatarSize
-import net.folivo.trixnity.client.MatrixClient
-import net.folivo.trixnity.client.media
-import net.folivo.trixnity.client.store.RoomUser
-import net.folivo.trixnity.client.store.avatarUrl
 import net.folivo.trixnity.core.model.UserId
-import net.folivo.trixnity.core.model.events.m.Presence
-import net.folivo.trixnity.utils.toByteArray
 
 data class UserInfoElement(
     val name: String,
     val initials: String? = null,
     val image: ByteArray? = null,
+    val userId: UserId? = null,
 ) {
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
@@ -27,13 +20,15 @@ data class UserInfoElement(
             if (other.image == null) return false
             if (!image.contentEquals(other.image)) return false
         } else if (other.image != null) return false
+        if (userId != other.userId) return false
         return true
     }
 
     override fun hashCode(): Int {
         var result = name.hashCode()
-        result = 31 * result + initials.hashCode()
+        result = 31 * result + (initials?.hashCode() ?: 0)
         result = 31 * result + (image?.contentHashCode() ?: 0)
+        result = 31 * result + (userId?.hashCode() ?: 0)
         return result
     }
 }
