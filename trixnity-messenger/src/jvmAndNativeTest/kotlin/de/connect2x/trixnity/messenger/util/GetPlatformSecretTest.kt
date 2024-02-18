0717d269d36e98f48dae3b30544fac5bbf6690a1
benkuly <kontakt@folivo.net>
2024-02-18 13:30:25 +0100

    adds missing GetPlatformSecret for apple targets

---
+++ trixnity-messenger/src/jvmAndNativeTest/kotlin/de/connect2x/trixnity/messenger/util/GetPlatformSecretTest.kt
@@ -0,0 +1,25 @@
+package de.connect2x.trixnity.messenger.util
+
+import io.kotest.core.spec.style.ShouldSpec
+import io.kotest.matchers.nulls.shouldNotBeNull
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNotBe
+import org.koin.dsl.koinApplication
+
+class GetPlatformSecretTest : ShouldSpec({
+    val cut = koinApplication {
+        modules(platformGetPlatformSecret())
+    }.koin.get<GetPlatformSecret>()
+
+    val id: String = GetPlatformSecretTest::class.qualifiedName ?: "trixnity-messenger-GetPlatformSecretTest"
+
+    // enable for local testing
+    xshould("create and get secret") {
+        val createdSecret = cut(id, 32).shouldNotBeNull()
+        createdSecret.size shouldBe 32
+
+        cut(id, 16) shouldBe createdSecret
+
+        cut("$id-other", 32) shouldNotBe createdSecret
+    }
+})
