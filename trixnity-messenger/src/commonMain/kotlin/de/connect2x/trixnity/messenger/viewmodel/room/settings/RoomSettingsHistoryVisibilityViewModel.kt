c4252dc187eebd2f47f36778ed4afa84293f849e
fabian.patzwall <patzwall.fabian@gmail.com>
2024-03-12 14:59:06 +0100

    functionality added

--- trixnity-messenger/src/commonMain/kotlin/de/connect2x/trixnity/messenger/viewmodel/room/settings/RoomSettingsHistoryVisibilityViewModel.kt
+++ trixnity-messenger/src/commonMain/kotlin/de/connect2x/trixnity/messenger/viewmodel/room/settings/RoomSettingsHistoryVisibilityViewModel.kt
@@ -1,11 +1,15 @@
 package de.connect2x.trixnity.messenger.viewmodel.room.settings
 
 import de.connect2x.trixnity.messenger.viewmodel.MatrixClientViewModelContext
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
+import net.folivo.trixnity.client.room
+import net.folivo.trixnity.client.room.getState
+import net.folivo.trixnity.client.user
+import net.folivo.trixnity.client.user.canSendEvent
 import net.folivo.trixnity.core.model.RoomId
 import net.folivo.trixnity.core.model.events.m.room.HistoryVisibilityEventContent
+import net.folivo.trixnity.core.model.events.m.room.PowerLevelsEventContent
 
 interface RoomSettingsHistoryVisibilityViewModelFactory {
     fun create(
@@ -14,7 +18,7 @@ interface RoomSettingsHistoryVisibilityViewModelFactory {
         historyVisibility: HistoryVisibilityEventContent.HistoryVisibility,
         error: MutableStateFlow<String?>,
     ): RoomSettingsHistoryVisibilityViewModel {
-        return RoomSettingsHistoryVisibilityViewModelImpl(viewModelContext, selectedRoomId, historyVisibility, error)
+        return RoomSettingsHistoryVisibilityViewModelImpl(viewModelContext, selectedRoomId, error)
     }
     companion object : RoomSettingsHistoryVisibilityViewModelFactory
 }
@@ -26,42 +30,51 @@ interface RoomSettingsHistoryVisibilityViewModel {
     fun changeRoomHistoryVisibility(newVisibility: HistoryVisibilityEventContent.HistoryVisibility)
 }
 
-open class RoomSettingsHistoryVisibilityViewModelImpl(
+class RoomSettingsHistoryVisibilityViewModelImpl(
     private val viewModelContext: MatrixClientViewModelContext,
     private val selectedRoomId: RoomId,
-    historyVisibility: HistoryVisibilityEventContent.HistoryVisibility,
     private val error: MutableStateFlow<String?>,
 ) : MatrixClientViewModelContext by viewModelContext, RoomSettingsHistoryVisibilityViewModel {
-
-    private val _roomHistoryVisibilityState = MutableStateFlow(historyVisibility)
-    override val roomHistoryVisibility: StateFlow<HistoryVisibilityEventContent.HistoryVisibility> = _roomHistoryVisibilityState
-    override val canChangeRoomHistoryVisibility = MutableStateFlow(false)
+    override val roomHistoryVisibility = matrixClient.room.getState<HistoryVisibilityEventContent>(selectedRoomId)
+        .map { it?.content?.historyVisibility ?: HistoryVisibilityEventContent.HistoryVisibility.SHARED }
+        .stateIn(
+            coroutineScope,
+            SharingStarted.WhileSubscribed(),
+            HistoryVisibilityEventContent.HistoryVisibility.SHARED
+        )
+    override val canChangeRoomHistoryVisibility =
+        matrixClient.user.canSendEvent<HistoryVisibilityEventContent>(selectedRoomId)
+            .stateIn(coroutineScope, SharingStarted.WhileSubscribed(), false)
 
     override fun changeRoomHistoryVisibility(newVisibility: HistoryVisibilityEventContent.HistoryVisibility) {
-        viewModelContext.coroutineScope.launch {
-            try {
-                viewModelContext.matrixClient.api.room.sendStateEvent(
-                    selectedRoomId,
-                    HistoryVisibilityEventContent(newVisibility),
-                    stateKey = ""
-                )
-                    .onFailure {
-                        error.value = "Failed to change room history visibility: ${it.message}"
-                    }
-                    .onSuccess {
-                        _roomHistoryVisibilityState.value = newVisibility
-                        error.value = null
-                    }
-            } catch (e: Exception) {
-                error.value = "Exception occurred while changing room history visibility: ${e.message}"
+        if (canChangeRoomHistoryVisibility.value) {
+            viewModelContext.coroutineScope.launch {
+                try {
+                    viewModelContext.matrixClient.api.room.sendStateEvent(
+                        selectedRoomId,
+                        HistoryVisibilityEventContent(newVisibility),
+                        stateKey = ""
+                    )
+                        .onFailure {
+                            error.value = "Failed to change room history visibility: ${it.message}"
+                        }
+                        .onSuccess {
+                            error.value = null
+                        }
+                } catch (e: Exception) {
+                    error.value = "Exception occurred while changing room history visibility: ${e.message}"
+                }
             }
+        }
+        else {
+            error.value = "Insufficient power level to change room history visibility"
         }
     }
 }
 
 class PreviewRoomSettingsHistoryVisibilityViewModel : RoomSettingsHistoryVisibilityViewModel {
     override val roomHistoryVisibility = MutableStateFlow(HistoryVisibilityEventContent.HistoryVisibility.SHARED)
-    override val canChangeRoomHistoryVisibility = MutableStateFlow(true)
+    override val canChangeRoomHistoryVisibility = MutableStateFlow(false)
 
     override fun changeRoomHistoryVisibility(newVisibility: HistoryVisibilityEventContent.HistoryVisibility) {
         roomHistoryVisibility.value = newVisibility
