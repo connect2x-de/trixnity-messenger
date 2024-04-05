b0fa6d7fac6594358107880121f9b09d68e376f1
jhaman <jhamank7@gmail.com>
2024-04-05 14:14:50 +0200

    Fixed usedId in sender object.

--- trixnity-messenger/src/commonMain/kotlin/de/connect2x/trixnity/messenger/viewmodel/room/timeline/elements/TimelineElementHolderViewModel.kt
+++ trixnity-messenger/src/commonMain/kotlin/de/connect2x/trixnity/messenger/viewmodel/room/timeline/elements/TimelineElementHolderViewModel.kt
@@ -230,7 +230,7 @@ open class TimelineElementHolderViewModelImpl(
                         UserInfoElement(
                             name = user?.name ?: timelineEvent.event.sender.full,
                             initials = user?.name?.let(initials::compute),
-                            userId = user?.userId,
+                            userId = user?.userId?: timelineEvent.event.sender,
                             image = user?.avatarUrl?.let { avatarUrl ->
                                 matrixClient.media.getThumbnail(
                                     avatarUrl,
