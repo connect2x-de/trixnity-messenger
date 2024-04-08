803c102a24b7dd25555dc8a828f36f671a87b43c
benkuly <kontakt@folivo.net>
2024-04-08 17:11:10 +0200

    Revert "new version scheme"
    This reverts commit eb9a45b7

--- buildSrc/src/main/kotlin/utils.kt
+++ buildSrc/src/main/kotlin/utils.kt
@@ -1,25 +1,12 @@
-import java.time.Instant
-import java.time.ZoneId
-import java.time.format.DateTimeFormatter
-
 val isCI = System.getenv("CI") != null
 val isRelease = System.getenv("CI_COMMIT_TAG")?.matches("^v\\d+.\\d+.\\d+.*".toRegex()) ?: false
-fun withVersionSuffix(version: String) = when {
-    isRelease -> {
-        val commitTagVersion = System.getenv("CI_COMMIT_TAG").removePrefix("v")
-        check(version == commitTagVersion.substringBefore("-")) {
-            "version from code ($version) does not match commit tag version ($commitTagVersion)"
-        }
-        commitTagVersion
-    }
 
-    isCI -> {
-        val formatter = DateTimeFormatter.ofPattern("yyMMddHHmmss")
-            .withZone(ZoneId.systemDefault())
-        val instant = Instant.parse(System.getenv("CI_COMMIT_TIMESTAMP"))
-        val formattedInstant = formatter.format(instant)
-        "$version-SNAPSHOT-$formattedInstant"
-    }
-
-    else -> "$version-LOCAL"
+fun checkSameReleaseVersion(version: String) {
+    if (isRelease) check(version == System.getenv("CI_COMMIT_TAG").removePrefix("v"))
 }
+
+fun withVersionSuffix(version: String) = (version + when {
+    isRelease -> ""
+    isCI -> "-SNAPSHOT-" + System.getenv("CI_COMMIT_SHORT_SHA")
+    else -> "-LOCAL"
+}).also { checkSameReleaseVersion(it) }
