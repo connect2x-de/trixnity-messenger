22fe5c621aee44a4e3addeaa35fa452a65d31b93
benkuly <kontakt@folivo.net>
2024-04-23 12:09:08 +0200

    updated trixnity and added some logging

--- trixnity-messenger/src/appleMain/kotlin/de/connect2x/trixnity/messenger/util/UriCaller.apple.kt
+++ trixnity-messenger/src/appleMain/kotlin/de/connect2x/trixnity/messenger/util/UriCaller.apple.kt
@@ -1,12 +1,18 @@
 package de.connect2x.trixnity.messenger.util
 
+import io.github.oshai.kotlinlogging.KotlinLogging
 import org.koin.core.module.Module
 import org.koin.dsl.module
 import platform.Foundation.NSURL.Companion.URLWithString
 import platform.UIKit.UIApplication
 
+private val log = KotlinLogging.logger { }
+
 actual fun platformUriCallerModule(): Module = module {
     single<UriCaller> {
-        UriCaller { uri -> UIApplication.sharedApplication.openURL(checkNotNull(URLWithString(uri))) }
+        UriCaller { uri ->
+            log.info { "call uri: $it" }
+            UIApplication.sharedApplication.openURL(checkNotNull(URLWithString(uri)))
+        }
     }
 }
--- trixnity-messenger/src/jsMain/kotlin/de/connect2x/trixnity/messenger/util/UriCaller.js.kt
+++ trixnity-messenger/src/jsMain/kotlin/de/connect2x/trixnity/messenger/util/UriCaller.js.kt
@@ -1,11 +1,17 @@
 package de.connect2x.trixnity.messenger.util
 
+import io.github.oshai.kotlinlogging.KotlinLogging
 import org.koin.core.module.Module
 import org.koin.dsl.module
 import web.window.window
 
+private val log = KotlinLogging.logger { }
+
 actual fun platformUriCallerModule(): Module = module {
     single<UriCaller> {
-        UriCaller { uri -> window.open(uri) }
+        UriCaller { uri ->
+            log.info { "call uri: $it" }
+            window.open(uri)
+        }
     }
 }
