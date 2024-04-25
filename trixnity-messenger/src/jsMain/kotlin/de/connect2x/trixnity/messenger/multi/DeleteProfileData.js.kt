605f3a1f00778cd302d590f848e3cd6de38ea439
jhaman <jhamank7@gmail.com>
2024-04-25 17:06:52 +0200

    Search all databased and deleted

--- trixnity-messenger/src/jsMain/kotlin/de/connect2x/trixnity/messenger/multi/DeleteProfileData.js.kt
+++ trixnity-messenger/src/jsMain/kotlin/de/connect2x/trixnity/messenger/multi/DeleteProfileData.js.kt
@@ -1,16 +1,24 @@
 package de.connect2x.trixnity.messenger.multi
 
-import com.juul.indexeddb.deleteDatabase
 import de.connect2x.trixnity.messenger.util.StoragePrefix
 import org.koin.core.module.Module
 import org.koin.dsl.module
+import web.idb.indexedDB
 
 internal actual fun platformDeleteProfileDataModule(): Module = module {
     single<DeleteProfileData> {
         val storagePrefix = get<StoragePrefix>().storagePrefix
         DeleteProfileData { profile ->
-            deleteDatabase("$storagePrefix$profile/")
-            deleteDatabase("$storagePrefix$profile/")
+            val databaseNamesPromise = indexedDB.databases()
+            databaseNamesPromise.then { databases ->
+                databases.forEach { database ->
+                    database.name?.let { databaseName ->
+                        if (databaseName.startsWith("$storagePrefix$profile/")) {
+                            indexedDB.deleteDatabase(databaseName)
+                        }
+                    }
+                }
+            }
         }
     }
 }
