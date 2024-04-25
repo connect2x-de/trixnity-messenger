9cd5bbd4a931586b6cd41600f05792825cc6f54c
jhaman <jhamank7@gmail.com>
2024-04-25 17:13:36 +0200

    Added await to prevent to used of promosies.

--- trixnity-messenger/src/jsMain/kotlin/de/connect2x/trixnity/messenger/multi/DeleteProfileData.js.kt
+++ trixnity-messenger/src/jsMain/kotlin/de/connect2x/trixnity/messenger/multi/DeleteProfileData.js.kt
@@ -1,6 +1,7 @@
 package de.connect2x.trixnity.messenger.multi
 
 import de.connect2x.trixnity.messenger.util.StoragePrefix
+import js.promise.await
 import org.koin.core.module.Module
 import org.koin.dsl.module
 import web.idb.indexedDB
@@ -9,13 +10,11 @@ internal actual fun platformDeleteProfileDataModule(): Module = module {
     single<DeleteProfileData> {
         val storagePrefix = get<StoragePrefix>().storagePrefix
         DeleteProfileData { profile ->
-            val databaseNamesPromise = indexedDB.databases()
-            databaseNamesPromise.then { databases ->
-                databases.forEach { database ->
-                    database.name?.let { databaseName ->
-                        if (databaseName.startsWith("$storagePrefix$profile/")) {
-                            indexedDB.deleteDatabase(databaseName)
-                        }
+            val allDatabases = indexedDB.databases().await()
+            allDatabases.forEach { database ->
+                database.name?.let { databaseName ->
+                    if (databaseName.startsWith("$storagePrefix$profile/")) {
+                        indexedDB.deleteDatabase(databaseName)
                     }
                 }
             }
