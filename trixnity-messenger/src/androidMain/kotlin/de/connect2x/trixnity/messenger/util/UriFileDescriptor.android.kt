5cedc4093c70544a593a632126fd02f5500f045d
jhaman <jhamank7@gmail.com>
2024-05-24 08:39:09 +0200

    Addresses comments for Android FileDescriptor

--- trixnity-messenger/src/androidMain/kotlin/de/connect2x/trixnity/messenger/util/UriFileDescriptor.android.kt
+++ trixnity-messenger/src/androidMain/kotlin/de/connect2x/trixnity/messenger/util/UriFileDescriptor.android.kt
@@ -6,29 +6,23 @@ import android.provider.OpenableColumns
 import io.ktor.http.ContentType
 import io.ktor.http.fromFilePath
 import net.folivo.trixnity.utils.ByteArrayFlow
-import net.folivo.trixnity.utils.toByteArrayFlow
+import net.folivo.trixnity.utils.byteArrayFlowFromSource
+import okio.Buffer
+import okio.source
 
-class ManualFileDescriptor(
-    val context: Context,
-    private val fileUri: Uri,
-) : FileDescriptor {
-
+class UriFileDescriptor(private val context: Context, private val fileUri: Uri) : FileDescriptor {
 
     private val fileDataInfo = getFileNameAndSize(fileUri)
-    override val fileName: String
-        get() = fileDataInfo?.first ?: "Unknown"
-    override val fileSize: Int?
-        get() = fileDataInfo?.second
-    override val mimeType: ContentType?
-        get() = ContentType.fromFilePath(fileDataInfo?.first ?: "").firstOrNull()
-    override val content: ByteArrayFlow
-        get() = toByteArrayFlow(fileUri)
+    override val fileName: String = fileDataInfo?.first ?: "Unknown"
+    override val fileSize: Int? = fileDataInfo?.second
+    override val mimeType: ContentType? = ContentType.fromFilePath(fileDataInfo?.first ?: "").firstOrNull()
+    override val content: ByteArrayFlow =
+        byteArrayFlowFromSource { context.contentResolver.openInputStream(fileUri)?.source() ?: Buffer() }
 
     private fun getFileNameAndSize(uri: Uri): Pair<String, Int?>? = runCatching {
         context.contentResolver.query(uri, null, null, null, null)?.use { cursor ->
             val nameIndex = cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME)
             val sizeIndex = cursor.getColumnIndex(OpenableColumns.SIZE)
-
             if (cursor.moveToFirst()) {
                 return@use Pair(cursor.getString(nameIndex), cursor.getLong(sizeIndex).toInt())
             } else {
@@ -36,10 +30,4 @@ class ManualFileDescriptor(
             }
         }
     }.getOrNull()
-
-    private fun toByteArrayFlow(uri: Uri): ByteArrayFlow =
-        context.contentResolver.openInputStream(uri).use {
-            val byteArray = it?.buffered()?.readBytes() ?: ByteArray(0)
-            byteArray.toByteArrayFlow()
-        }
 }
