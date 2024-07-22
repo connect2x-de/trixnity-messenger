5d54ea61b53baf6af8b6f008047df4d5b0b64f41
KitsuneAlex <alexh@karmakrafts.dev>
2024-07-22 15:14:32 +0200

    Explicitly set Java toolchain version(s) for emoji subproject

--- emojis/build.gradle.kts
+++ emojis/build.gradle.kts
@@ -1,7 +1,21 @@
 plugins {
     id("application")
     kotlin("jvm")
-    kotlin("plugin.serialization").version(libs.versions.kotlin.get()) // for creation of Emojis.kt
+    kotlin("plugin.serialization") version libs.versions.kotlin.get() // for creation of Emojis.kt
+}
+
+val jvmTarget = libs.versions.kotlinJvmTarget.get().toInt()
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(jvmTarget)
+    }
+    sourceCompatibility = JavaVersion.toVersion(jvmTarget)
+    targetCompatibility = sourceCompatibility
+}
+
+kotlin {
+    jvmToolchain(jvmTarget)
 }
 
 application {
@@ -10,8 +24,4 @@ application {
 
 dependencies {
     implementation(libs.kotlinx.serialization)
-}
-
-kotlin {
-    jvmToolchain(libs.versions.kotlinJvmTarget.get().toInt())
 }
