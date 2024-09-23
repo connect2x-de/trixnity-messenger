77c9d7f0ffa28498adb3e5b2e45fa70defec863d
benkuly <kontakt@folivo.net>
2024-09-23 08:40:08 +0200

    bump version

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,7 +28,7 @@ plugins {
 
 allprojects {
     group = "de.connect2x"
-    version = withVersionSuffix("2.2.1")
+    version = withVersionSuffix("2.2.2")
 
     repositories {
         mavenCentral()
