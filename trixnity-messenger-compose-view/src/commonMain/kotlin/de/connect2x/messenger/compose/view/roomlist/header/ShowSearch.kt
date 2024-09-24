6eb6f6a444a5b899a32c9f0b58292e05b069ac0e
jlddorf <jakob.deutschendorf@gmail.com>
2024-09-25 01:05:59 +0200

    formatting

--- trixnity-messenger-compose-view/src/commonMain/kotlin/de/connect2x/messenger/compose/view/roomlist/header/ShowSearch.kt
+++ trixnity-messenger-compose-view/src/commonMain/kotlin/de/connect2x/messenger/compose/view/roomlist/header/ShowSearch.kt
@@ -3,7 +3,6 @@ package de.connect2x.messenger.compose.view.roomlist.header
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.filled.SearchOff
-import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
@@ -34,15 +33,18 @@ class ShowSearchViewImpl : ShowSearchView {
         if (showSearch.value) {
             Tooltip(
                 tooltip = { TooltipText(i18n.accountDeactivateSearch()) }) {
-                IconButton(onClick = {roomListViewModel.showSearch.value = false},
+                IconButton(
+                    onClick = { roomListViewModel.showSearch.value = false },
                 ) {
                     Icon(Icons.Default.SearchOff, i18n.accountActivateSearch())
-                }            }
+                }
+            }
         } else {
             Tooltip(
                 tooltip = { TooltipText(i18n.accountActivateSearch()) }) {
-                IconButton(onClick = {roomListViewModel.showSearch.value = true},
-                    ) {
+                IconButton(
+                    onClick = { roomListViewModel.showSearch.value = true },
+                ) {
                     Icon(Icons.Default.Search, i18n.accountActivateSearch())
                 }
             }
