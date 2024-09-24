25558c8e971e0ca55f86d3f0011a1a0ad2f3872f
jlddorf <jakob.deutschendorf@gmail.com>
2024-09-24 23:48:40 +0200

    add tooltip to web and add onClick to android tooltips

--- trixnity-messenger-compose-view/src/androidMain/kotlin/de/connect2x/messenger/compose/view/Platform.kt
+++ trixnity-messenger-compose-view/src/androidMain/kotlin/de/connect2x/messenger/compose/view/Platform.kt
@@ -5,6 +5,7 @@ import android.content.ClipboardManager
 import android.content.Context
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.ScrollState
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
@@ -13,13 +14,9 @@ import androidx.compose.material3.TooltipBox
 import androidx.compose.material3.TooltipDefaults
 import androidx.compose.material3.rememberTooltipState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.core.content.ContextCompat.getSystemService
-import de.connect2x.messenger.compose.view.theme.messengerColors
 import io.github.oshai.kotlinlogging.KotlinLogging
-import kotlinx.coroutines.launch
 import org.koin.core.Koin
 
 private val log = KotlinLogging.logger { }
@@ -63,7 +60,7 @@ actual fun Tooltip(
         positionProvider = TooltipDefaults.rememberPlainTooltipPositionProvider(),
         tooltip = {
             PlainTooltip(
-                modifier = Modifier,
+                modifier = modifier.clickable(onClick = onClick ?: { }),
                 caretSize = TooltipDefaults.caretSize,
                 contentColor = MaterialTheme.colorScheme.onPrimary,
                 containerColor = MaterialTheme.colorScheme.onPrimaryContainer,
