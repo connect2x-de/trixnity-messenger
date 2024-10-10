58d656bc181e2a700b57471f892303cc7822109d
michael.thiele <thiele.michael2@gmail.com>
2024-10-10 14:10:55 +0200

    add support for AWT images

--- trixnity-messenger-compose-view/src/desktopMain/kotlin/de/connect2x/messenger/compose/view/files/actuals.kt
+++ trixnity-messenger-compose-view/src/desktopMain/kotlin/de/connect2x/messenger/compose/view/files/actuals.kt
@@ -64,6 +64,7 @@ import io.ktor.http.*
 import net.folivo.trixnity.utils.ByteArrayFlow
 import net.folivo.trixnity.utils.byteArrayFlowFromInputStream
 import net.folivo.trixnity.utils.nextString
+import net.folivo.trixnity.utils.toByteArrayFlow
 import net.folivo.trixnity.utils.write
 import okio.FileSystem
 import okio.Path.Companion.toPath
@@ -75,10 +76,13 @@ import java.awt.Toolkit
 import java.awt.datatransfer.Clipboard
 import java.awt.datatransfer.DataFlavor
 import java.awt.datatransfer.UnsupportedFlavorException
+import java.awt.image.BufferedImage
+import java.io.ByteArrayOutputStream
 import java.io.IOException
 import java.io.InputStream
 import java.io.Reader
 import java.net.URI
+import javax.imageio.ImageIO
 import kotlin.math.max
 import kotlin.math.min
 import kotlin.random.Random
@@ -236,6 +240,7 @@ private val uriListContentType = ContentType.parse("text/uri-list")
 
 private interface ClipboardType {
     data object Image : ClipboardType
+    data object AwtImage : ClipboardType
     data object UriList : ClipboardType
 }
 
@@ -272,13 +277,14 @@ actual fun getClipboardFile(fileSystem: FileSystem): FileDescriptor? {
     clipboard.availableDataFlavors.forEach { flavor ->
         val contentType = ContentType.parse(flavor.mimeType)
 
-        val clipboardType = if (isPreviewableImage(contentType)) {
-            ClipboardType.Image
-        } else if (contentType.match(uriListContentType)) {
-            ClipboardType.UriList
-        } else {
-            log.trace { "unknown clipboard flavor: ${flavor.humanPresentableName}" }
-            return@forEach
+        val clipboardType = when {
+            isPreviewableImage(contentType) -> ClipboardType.Image
+            flavor.representationClass == java.awt.Image::class.java -> ClipboardType.AwtImage
+            contentType.match(uriListContentType) -> ClipboardType.UriList
+            else -> {
+                log.trace { "unknown clipboard flavor: ${flavor.humanPresentableName}" }
+                return@forEach
+            }
         }
 
         if (clipboardType == ClipboardType.Image && !flavor.isRepresentationClassInputStream) {
@@ -305,6 +311,31 @@ actual fun getClipboardFile(fileSystem: FileSystem): FileDescriptor? {
                         clipboard.getDataOrNull<InputStream>(flavor) ?: InputStream.nullInputStream()
                     }
                 )
+            }
+
+            ClipboardType.AwtImage -> {
+                clipboard.getDataOrNull<java.awt.Image>(flavor)?.let { img ->
+                    // this might not be the most efficient way, but works for images in memory on MacOS...
+                    val image = BufferedImage(img.getWidth(null), img.getHeight(null), BufferedImage.TYPE_INT_ARGB)
+                    image.createGraphics().apply {
+                        drawImage(img, 0, 0, null)
+                        dispose()
+                    }
+                    val outputStream = ByteArrayOutputStream()
+                    ImageIO.write(image, "png", outputStream)
+                    outputStream.flush()
+                    val byteArray = outputStream.toByteArray()
+                    outputStream.close()
+
+                    val baseName = Random.nextString(12)
+                    val extStr = contentType.fileExtensions().firstOrNull()?.let { ".$it" } ?: ""
+                    return BasicFileDescriptor(
+                        baseName + extStr,
+                        byteArray.size.toLong(),
+                        contentType,
+                        byteArray.toByteArrayFlow(),
+                    )
+                }
             }
 
             ClipboardType.UriList -> {
