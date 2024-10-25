3cdb28dac7bb17fad622eba5ff886acb5a7b3a75
Felix Hilgers <contact@fhilgers.com>
2024-10-25 09:59:50 +0200

    implement actuals for the info popup

---
+++ trixnity-messenger-compose-view/src/desktopMain/kotlin/de/connect2x/messenger/compose/view/common/InfoPopup.desktop.kt
@@ -0,0 +1,62 @@
+package de.connect2x.messenger.compose.view.common
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.MutableTransitionState
+import androidx.compose.animation.expandVertically
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.shrinkVertically
+import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.input.key.Key
+import androidx.compose.ui.input.key.key
+import androidx.compose.ui.input.key.onKeyEvent
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Popup
+import androidx.compose.ui.window.PopupProperties
+
+@Composable
+actual fun InfoPopup(
+    isOpen: Boolean,
+    focusRequester: FocusRequester,
+    onDismiss: () -> Unit,
+    readers: List<String>,
+    modifier: Modifier,
+) {
+    val expandedState = remember { MutableTransitionState(false) }
+    expandedState.targetState = isOpen
+
+    if (expandedState.currentState || expandedState.targetState || !expandedState.isIdle) {
+        Popup(
+            onDismissRequest = onDismiss,
+            alignment = Alignment.BottomEnd,
+            properties = PopupProperties(
+                focusable = true,
+            ),
+        ) {
+            AnimatedVisibility(
+                visibleState = expandedState,
+                enter = fadeIn() + expandVertically(),
+                exit = fadeOut() + shrinkVertically(),
+            ) {
+                Surface(
+                    Modifier.size(320.dp, 240.dp),
+                    shadowElevation = 4.dp,
+                    tonalElevation = 4.dp,
+                    shape = MaterialTheme.shapes.medium
+                ) {
+                    Info(readers, focusRequester)
+                }
+            }
+        }
+    }
+}
