b71d3909b8a675b977b4501ded1957dfc85e6303
adambrangenberg <adabran06@gmail.com>
2024-11-01 17:42:42 +0100

    Switching to ActivityGetter

--- trixnity-messenger/src/androidMain/kotlin/de/connect2x/trixnity/messenger/util/MinimiseApp.android.kt
+++ trixnity-messenger/src/androidMain/kotlin/de/connect2x/trixnity/messenger/util/MinimiseApp.android.kt
@@ -1,14 +1,13 @@
 package de.connect2x.trixnity.messenger.util
 
-import android.app.Activity
 import org.koin.core.module.Module
 import org.koin.dsl.module
 
 actual fun platformMinimizeAppModule(): Module = module {
     single<MinimizeApp> {
-        val activity = get<Activity>()
+        val activityGetter = get<ActivityGetter>()
         MinimizeApp {
-            activity.moveTaskToBack(true)
+            activityGetter()?.moveTaskToBack(true)
         }
     }
 }
