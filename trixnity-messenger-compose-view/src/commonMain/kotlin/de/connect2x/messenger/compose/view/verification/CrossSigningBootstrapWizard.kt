2e9c7e957a7a416adb0065a72f5230e73068564b
Felix Hilgers <contact@fhilgers.com>
2024-11-22 10:27:04 +0100

    Automatically proceed bootstrapping without next button

--- trixnity-messenger-compose-view/src/commonMain/kotlin/de/connect2x/messenger/compose/view/verification/CrossSigningBootstrapWizard.kt
+++ trixnity-messenger-compose-view/src/commonMain/kotlin/de/connect2x/messenger/compose/view/verification/CrossSigningBootstrapWizard.kt
@@ -23,6 +23,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -102,10 +103,15 @@ fun CrossSigningBootstrapWizard(crossSigningBootstrapViewModel: CrossSigningBoot
                 }
             },
             nextButton = {
-                WizardNavigationButton.Standard(enabled = {
+                WizardNavigationButton.Custom {
                     val recoveryKey = crossSigningBootstrapViewModel.recoveryKey.collectAsState().value
-                    recoveryKey != null
-                })
+
+                    LaunchedEffect(recoveryKey) {
+                        if (recoveryKey != null) {
+                            nextStep?.let { currentStepId.value = it }
+                        }
+                    }
+                }
             }
         ),
 
