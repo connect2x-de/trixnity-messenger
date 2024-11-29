702ccfad7e8f1aaa251bddb3c3ce955c572c388b
michael.thiele <thiele.michael2@gmail.com>
2024-11-29 09:43:33 +0100

    use git show-ref instead of git tag

--- kmmPublish.sh
+++ kmmPublish.sh
@@ -2,10 +2,11 @@
 
 TAG=$(./gradlew properties --no-daemon --console=plain -q | grep "^version:" | awk '{printf $2}' | sed 's/DEV-/alpha./g')
 git clone https://write_to_repo:"$SPM_PROJECT_ACCESS_TOKEN"@gitlab.com/connect2x/trixnity-messenger/spm &&
-cd spm
-NEW_TAG=$(git tag | grep "$TAG")
-if [ "$NEW_TAG" = '' ]; then
-    cd ..
+cd spm &&
+if [ "$(git show-ref --tags "$TAG" --quiet)" ]; then
+    echo "Tag '$TAG' already exists, so do nothing."
+else
+    cd .. &&
     ./gradlew kmmBridgePublish updatePackageSwift --stacktrace &&
     cat Package.swift &&
     cp Package.swift spm/Package.swift &&
@@ -14,6 +15,4 @@ if [ "$NEW_TAG" = '' ]; then
     git commit -m "update to version $TAG" &&
     git tag "$TAG" &&
     git push origin --atomic main tag "$TAG"
-else
-    echo "Tag '$TAG' already exists, so do nothing."
 fi
