f2207a375899f32e60a76c25d75dac1718fd2b8d
Felix Hilgers <contact@fhilgers.com>
2025-01-14 14:43:29 +0100

    Enable room database encryption on jvm target

--- trixnity-messenger/src/jvmMain/kotlin/de/connect2x/trixnity/messenger/CreateRepositoriesModule.jvm.kt
+++ trixnity-messenger/src/jvmMain/kotlin/de/connect2x/trixnity/messenger/CreateRepositoriesModule.jvm.kt
@@ -2,13 +2,17 @@ package de.connect2x.trixnity.messenger
 
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.sqlite.SQLiteConnection
+import androidx.sqlite.SQLiteDriver
 import androidx.sqlite.driver.bundled.BundledSQLiteDriver
+import androidx.sqlite.use
 import de.connect2x.trixnity.messenger.util.ConvertSecretByteArray
 import de.connect2x.trixnity.messenger.util.RootPath
 import de.connect2x.trixnity.messenger.util.SecretByteArray
 import net.folivo.trixnity.client.store.repository.room.TrixnityRoomDatabase
 import net.folivo.trixnity.client.store.repository.room.createRoomRepositoriesModule
 import net.folivo.trixnity.core.model.UserId
+import net.folivo.trixnity.crypto.core.SecureRandom
 import org.koin.core.module.Module
 import org.koin.dsl.module
 
@@ -19,10 +23,11 @@ actual fun platformCreateRepositoriesModuleModule(): Module = module {
 
         object : CreateRepositoriesModule {
             override suspend fun create(userId: UserId): CreateRepositoriesModule.CreateResult {
-                val databaseKey: ByteArray? = null // FIXME
+                // TODO: figure out key size: sqlite3mc encrypts via passphrase and internally uses a kdf depending on cipher
+                val databaseKey = SecureRandom.nextBytes(64)
                 return CreateRepositoriesModule.CreateResult(
                     module = createRoomRepositoriesModule(db(userId, databaseKey)),
-                    databaseKey = databaseKey?.let { convertSecretByteArray(it) },
+                    databaseKey = convertSecretByteArray(databaseKey)
                 )
             }
 
@@ -38,8 +43,26 @@ actual fun platformCreateRepositoriesModuleModule(): Module = module {
                 Room.databaseBuilder<TrixnityRoomDatabase>(
                     rootPath.forAccountDatabase(userId).resolve("database").toString()
                 ).apply {
-                    setDriver(BundledSQLiteDriver())
+                    println("Creating database for user $userId with key $databaseKey")
+                    setDriver(databaseKey?.let(::EncryptedSQLiteDriver) ?: BundledSQLiteDriver())
                 }
         }
     }
+}
+
+private class EncryptedSQLiteDriver(key: ByteArray) : SQLiteDriver {
+
+    @ExperimentalStdlibApi
+    private val hexKey = key.toHexString()
+
+    private val driver = BundledSQLiteDriver()
+
+    @ExperimentalStdlibApi
+    override fun open(fileName: String): SQLiteConnection =
+        driver.open(fileName).apply {
+            prepare("PRAGMA hexkey = '$hexKey'").use {
+                if (!it.step() || it.getColumnNames().getOrNull(0) != "ok")
+                    throw Exception("Database does not support Encryption")
+            }
+        }
 }
