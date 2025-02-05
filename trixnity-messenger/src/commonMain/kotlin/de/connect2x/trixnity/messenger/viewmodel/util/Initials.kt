05db4596597fca5ea54ce6b9f636ad0a4e639748
KitsuneAlex <alexh@karmakrafts.dev>
2025-02-05 22:04:46 +0100

    Make initials computation fully Unicode compliant

--- trixnity-messenger/src/commonMain/kotlin/de/connect2x/trixnity/messenger/viewmodel/util/Initials.kt
+++ trixnity-messenger/src/commonMain/kotlin/de/connect2x/trixnity/messenger/viewmodel/util/Initials.kt
@@ -1,22 +1,20 @@
 package de.connect2x.trixnity.messenger.viewmodel.util
 
+import de.connect2x.trixnity.messenger.util.takeGraphemes
 import de.connect2x.trixnity.messenger.viewmodel.util.RegexUtil.matchBlankSpace
 
 
 interface Initials {
     fun compute(name: String): String =
         name.split(matchBlankSpace)
-            .asSequence()
             .map {
-                it.mapIndexed { i, char ->
-                    if (i == 0) char.uppercaseChar()
-                    else if (i == 1 && char.isLowSurrogate()) char
-                    else null
-                }
-                    .filterNotNull()
+                it.asSequence()
+                    .takeGraphemes(1)
                     .joinToString(separator = "")
+                    .uppercase()
             }
             .filter { it.isNotEmpty() }
+            .asSequence()
             .take(2)
             .joinToString(separator = "")
 
