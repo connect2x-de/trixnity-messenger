a4b82cf0028c133691e39e14de8d0a41685c67eb
KitsuneAlex <alexh@karmakrafts.dev>
2025-02-05 05:11:48 +0100

    Implement String.graphemeCount for iOS

--- trixnity-messenger/src/iosMain/kotlin/de/connect2x/trixnity/messenger/util/Strings.ios.kt
+++ trixnity-messenger/src/iosMain/kotlin/de/connect2x/trixnity/messenger/util/Strings.ios.kt
@@ -1,4 +1,20 @@
 package de.connect2x.trixnity.messenger.util
 
+import kotlinx.cinterop.ExperimentalForeignApi
+import platform.Foundation.NSMakeRange
+import platform.Foundation.NSString
+import platform.Foundation.NSStringEnumerationByComposedCharacterSequences
+import platform.Foundation.enumerateSubstringsInRange
+
+@OptIn(ExperimentalForeignApi::class)
 actual val String.graphemeCount: Int
-    get() = 0 // TODO: implement this using NSString functionality
+    get() {
+        var count = 0
+        (this as NSString).enumerateSubstringsInRange(
+            range = NSMakeRange(0U, length.toULong()),
+            options = NSStringEnumerationByComposedCharacterSequences
+        ) { _, _, _, _ ->
+            ++count
+        }
+        return count
+    }
