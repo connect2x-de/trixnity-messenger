09253b1c25fab6375a8774b52e29811e4a326364
KitsuneAlex <alexh@karmakrafts.dev>
2025-02-12 10:44:58 +0100

    Cover missing cases for String.isEmoji, split pattern into digestible chunks and add some sources

--- trixnity-messenger/src/commonMain/kotlin/de/connect2x/trixnity/messenger/util/Strings.kt
+++ trixnity-messenger/src/commonMain/kotlin/de/connect2x/trixnity/messenger/util/Strings.kt
@@ -1,7 +1,20 @@
 package de.connect2x.trixnity.messenger.util
 
+/*
+ * See https://unicode.org/reports/tr51/tr51-28.html,
+ * https://unicode.org/emoji/charts/full-emoji-list.html and
+ * https://unicode.org/emoji/charts/full-emoji-modifiers.html
+ * for the ranges.
+ */
+private const val SURROGATE_PAIR: String = """[\uD83C-\uDBFF\uDC00-\uDFFF]+"""
+private const val SYMBOLICS: String = """[\u203C-\u3299]|[\u00A9\u00AE\u2122\u3030]"""
+private const val KEYCAPS: String = """[\u0023-\u0039]\uFE0F?\u20E3"""
+private const val VAR_SELECTOR: String = """\uFE0F"""
+private const val FLAGS: String = """(?:\uD83C[\uDDE6-\uDDFF]){2}"""
+private const val DIACRITICS: String = """[\u0300-\u036F]"""
+
 private val emojiPattern: Regex =
-    Regex("^([\\uD83C-\\uDBFF\\uDC00-\\uDFFF]+|[\\u203C-\\u3299]|[\\u0023-\\u0039]\\uFE0F?\\u20E3|[\\u00A9\\u00AE\\u2122\\u3030]|\\uFE0F)+$")
+    Regex("^($SURROGATE_PAIR|$SYMBOLICS|$KEYCAPS|$FLAGS|$DIACRITICS|$VAR_SELECTOR)+$")
 
 expect val String.graphCount: Int
 
