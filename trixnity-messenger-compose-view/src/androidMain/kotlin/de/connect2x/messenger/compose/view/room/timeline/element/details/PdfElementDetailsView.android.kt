9d0ff43d68546cee6dd7b51b6337c4f0cc1522de
jlddorf <jakob.deutschendorf@gmail.com>
2025-03-12 12:44:13 +0100

    start rewriting android reader

--- trixnity-messenger-compose-view/src/androidMain/kotlin/de/connect2x/messenger/compose/view/room/timeline/element/details/PdfElementDetailsView.android.kt
+++ trixnity-messenger-compose-view/src/androidMain/kotlin/de/connect2x/messenger/compose/view/room/timeline/element/details/PdfElementDetailsView.android.kt
@@ -1,8 +1,8 @@
 package de.connect2x.messenger.compose.view.room.timeline.element.details
 
 import android.graphics.Bitmap
-import android.graphics.Canvas
 import android.graphics.pdf.PdfRenderer
+import android.graphics.pdf.PdfRenderer.Page.RENDER_MODE_FOR_DISPLAY
 import android.os.ParcelFileDescriptor
 import android.os.ParcelFileDescriptor.MODE_READ_ONLY
 import androidx.compose.foundation.Image
@@ -33,6 +33,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.layout.onSizeChanged
@@ -62,10 +63,15 @@ actual fun PDFReader(
     onError: (String?) -> Unit,
 ) {
     val i18nView = DI.current.get<I18nView>()
-    var reader by remember { mutableStateOf<PdfRender?>(null) }
+    val renderer = remember { mutableStateOf<PdfRenderer?>(null) }
     val density = LocalDensity.current.density
     var viewSize by remember { mutableStateOf(IntSize.Zero) }
-
+    val pageCacheSize = max(2f, min(16f, 8f / scale)).toInt()
+    val cache = remember {
+        mutableStateOf<MutableMap<String, Pair<Long, ImageBitmap>>>(mutableMapOf())
+    }
+    val dpPixel = remember { mutableStateOf<Int?>(null) }
+    val documentWidth = remember { mutableStateOf<Int?>(null) }
     val (temporaryFile, setTemporaryFile) = remember { mutableStateOf<OkioPlatformMedia.TemporaryFile?>(null) }
     LaunchedEffect(Unit) {
         val temporaryFileResult = (media as OkioPlatformMedia).getTemporaryFile()
@@ -73,9 +79,8 @@ actual fun PDFReader(
             val newTemporaryFile = temporaryFileResult.getOrThrow()
             try {
                 setTemporaryFile(newTemporaryFile)
-                reader = PdfRender(
-                    fileDescriptor = ParcelFileDescriptor.open(newTemporaryFile.path.toFile(), MODE_READ_ONLY),
-                )
+                renderer.value = PdfRenderer(ParcelFileDescriptor.open(newTemporaryFile.path.toFile(), MODE_READ_ONLY))
+                documentWidth.value = renderer.value?.openPage(0).use { it?.width }
             } catch (exception: Exception) {
                 onError(exception.message)
             }
@@ -83,36 +88,36 @@ actual fun PDFReader(
             onError(temporaryFileResult.exceptionOrNull()?.message)
         }
     }
-    LaunchedEffect(reader?.documentWidth, viewSize.width, scale, density) {
-        reader?.documentWidth?.toFloat()?.let {
+    LaunchedEffect(documentWidth.value, viewSize.width) {
+        documentWidth.value?.toFloat()?.let {
             val maxDpi = 1f / it * 1800f
-            reader?.dpi = (viewSize.width / it * scale / density * 2f).coerceAtMost(maxDpi)
+            val dpi = (viewSize.width / it * scale / density * 2f).coerceAtMost(maxDpi)
+            dpPixel.value = (dpi / it).toInt()
         }
     }
     DisposableEffect(Unit) {
         @OptIn(DelicateCoroutinesApi::class)
         onDispose {
             GlobalScope.launch { temporaryFile?.delete() }
-            reader?.close()
+            renderer.value?.close()
         }
     }
 
-    var readerError by remember { mutableStateOf<String?>(null) }
+    val readerError = remember { mutableStateOf<String?>(null) }
 
     Box(
         Modifier
             .fillMaxSize()
             .onSizeChanged { viewSize = it }
     ) {
-        if (readerError == null) {
-            reader?.let { pdfReader ->
+        if (readerError.value == null) {
+            renderer.value?.let { pdfReader ->
                 if (pdfReader.pageCount == 0) {
                     CenteredElement {
                         Text(i18nView.fileOverlayPreviewNotSupported())
                     }
                     return@let
-                }
-                if (pdfReader.documentWidth != null) {
+                } else {
                     val lazyListState = rememberLazyListState()
                     val horizontalScroll = rememberScrollState()
                     LazyColumn(
@@ -125,16 +130,33 @@ actual fun PDFReader(
                         state = lazyListState,
                         content = {
                             items(count = pdfReader.pageCount, key = { it }) { pageId ->
-                                pdfReader[pageId]?.pageContent?.let { img ->
-                                    Image(
-                                        bitmap = img,
-                                        contentDescription = i18nView.fileOverlayPdfPageDescriptor(pageId),
-                                        modifier = Modifier
-                                            .background(color = Color.White) // Avoid performance drops on transparent images.
-                                            .width(viewSize.width.dp / density * scale - 16.dp),
-                                        contentScale = ContentScale.FillWidth,
-                                    )
+                                val cacheKey = "$pageId:${dpPixel.value?.toInt()}"
+                                val currentPage = cache.value[cacheKey]?.second ?: run {
+                                    println("rendering page $pageId")
+                                    pdfReader.openPage(pageId).use { page ->
+                                        val bitmap = Bitmap.createBitmap(
+                                            dpPixel.value?:0,
+                                            page.height,
+                                            Bitmap.Config.ARGB_8888
+                                        )
+                                        page.render(bitmap, null, null, RENDER_MODE_FOR_DISPLAY)
+                                        val image = bitmap.asImageBitmap()
+                                        cache.value[cacheKey] = Pair(System.currentTimeMillis(), image)
+                                        cache.value.toList().sortedBy { it.second.first }
+                                            .subList(0, 0.coerceAtLeast(cache.value.size - pageCacheSize))
+                                            .forEach { cache.value.remove(it.first) }
+                                        image
+                                    }
                                 }
+
+                                Image(
+                                    bitmap = currentPage,
+                                    contentDescription = i18nView.fileOverlayPdfPageDescriptor(pageId),
+                                    modifier = Modifier
+                                        .background(color = Color.White) // Avoid performance drops on transparent images.
+                                        .width(viewSize.width.dp / density * scale - 16.dp),
+                                    contentScale = ContentScale.FillWidth,
+                                )
                             }
                         }
                     )
@@ -145,130 +167,22 @@ actual fun PDFReader(
                         horizontalScroll,
                     )
                 }
-            } ?: CenteredElement {
-                CircularProgressIndicator(Modifier.size(32.dp))
-            }
-        } else {
-            readerError?.let {
-                Column(
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.Center,
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .padding(32.dp)
-                        .background(Color.Cyan),
-                ) { Text(it) }
-            }
-        }
-    }
-}
-
-// https://medium.com/telepass-digital/how-to-show-a-pdf-with-jetpack-compose-74fc773adbd0
-private class PdfRender(private val fileDescriptor: ParcelFileDescriptor) {
-    private val pdfRenderer = PdfRenderer(fileDescriptor)
-    val pageCount get() = pdfRenderer.pageCount
-
-    private val pageCacheSize: Int
-        get() = max(2f, min(16f, 8f / (dpi ?: 1f))).toInt()
-
-    private var _dpi: Float? = null
-    var dpi: Float?
-        get() = _dpi
-        set(value) {
-            clearPageCache()
-            _dpi = value
-        }
-
-    private var pageCache = mutableMapOf<Int, Page>()
-    operator fun get(pageId: Int): Page? {
-        trimPageCache()
-        return try {
-            if (!pageCache.containsKey(pageId)) pageCache[pageId] = Page(
-                pageId = pageId,
-                pdfRenderer = pdfRenderer,
-                dpi = _dpi ?: 1f,
-                { pageCache.size },
-            )
-            pageCache[pageId]
-        } catch (e: Exception) {
-            log.error { e }
-            null
-        }
-    }
-
-    private fun trimPageCache() {
-        pageCache.toList().sortedBy { it.second.creationTime }
-            .subList(0, Math.max(0, pageCache.size - pageCacheSize))
-            .forEach { pageCache.remove(it.first) }
-    }
-
-    val documentWidth: Int?
-        get() = _documentWidth
-            ?: this[0]?.originalWidth?.also {
-                if (it > 0) _documentWidth = it
-            }
-    private var _documentWidth: Int? = null
-
-    fun close() {
-        clearPageCache()
-        pdfRenderer.close()
-        fileDescriptor.close()
-    }
-
-    private fun clearPageCache() {
-        pageCache.values.forEach {
-            it.recycle()
-        }
-        pageCache.clear()
-    }
-
-    class Page(
-        val pageId: Int,
-        private val pdfRenderer: PdfRenderer,
-        private val dpi: Float,
-        private val getCacheSize: () -> Int,
-    ) {
-        val pageContent get() = _pageContent.asImageBitmap()
-        private val _pageContent = createBitmap()
-        val creationTime = System.currentTimeMillis()
-        var originalWidth: Int = 0
-
-        private fun createBitmap(): Bitmap {
-            val newBitmap: Bitmap
-            pdfRenderer.openPage(pageId).use { currentPage ->
-                log.debug {
-                    "render pdf page $pageId " +
-                            "to bitmap (${currentPage.width}x${currentPage.height}) " +
-                            "at scale factor: $dpi " +
-                            "with ${getCacheSize()} pages already cached"
+            } //No Renderer yet -> Display a loading indicator
+                ?: CenteredElement {
+                    CircularProgressIndicator(Modifier.size(32.dp))
                 }
-                originalWidth = currentPage.width
-                newBitmap = createBlankBitmap(
-                    width = (currentPage.width * dpi).toInt(),
-                    height = (currentPage.height * dpi).toInt(),
-                )
-                currentPage.render(
-                    newBitmap, null, null,
-                    PdfRenderer.Page.RENDER_MODE_FOR_DISPLAY,
-                )
-            }
-            return newBitmap
-        }
+        } //Error when reading a file
+        else {
+            val error = readerError.value
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(32.dp)
+                    .background(Color.Cyan),
+            ) { Text(error.toString()) }
 
-        fun recycle() {
-            _pageContent.recycle()
-        }
-
-        private fun createBlankBitmap(
-            width: Int,
-            height: Int,
-        ): Bitmap = androidx.core.graphics.createBitmap(
-            width,
-            height,
-            Bitmap.Config.ARGB_8888,
-        ).apply {
-            val canvas = Canvas(this)
-            canvas.drawBitmap(this, 0f, 0f, null)
         }
     }
 }
