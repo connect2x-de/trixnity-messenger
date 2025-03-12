869fcab62cb5a7282ec5d8a7c8dfbb8f400dbd51
jlddorf <jakob.deutschendorf@gmail.com>
2025-03-12 13:05:31 +0100

    add dpi to android pdf calculation

--- trixnity-messenger-compose-view/src/androidMain/kotlin/de/connect2x/messenger/compose/view/room/timeline/element/details/PdfElementDetailsView.android.kt
+++ trixnity-messenger-compose-view/src/androidMain/kotlin/de/connect2x/messenger/compose/view/room/timeline/element/details/PdfElementDetailsView.android.kt
@@ -7,14 +7,14 @@ import android.os.ParcelFileDescriptor
 import android.os.ParcelFileDescriptor.MODE_READ_ONLY
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.TransformableState
+import androidx.compose.foundation.gestures.transformable
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
@@ -32,6 +32,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.asImageBitmap
@@ -60,6 +61,9 @@ private val log = KotlinLogging.logger {}
 actual fun PDFReader(
     media: PlatformMedia,
     scale: Float,
+    isZooming: Boolean,
+    state: TransformableState,
+    offset: Offset,
     onError: (String?) -> Unit,
 ) {
     val i18nView = DI.current.get<I18nView>()
@@ -70,7 +74,7 @@ actual fun PDFReader(
     val cache = remember {
         mutableStateOf<MutableMap<String, Pair<Long, ImageBitmap>>>(mutableMapOf())
     }
-    val dpPixel = remember { mutableStateOf<Int?>(null) }
+    val dpi = remember { mutableStateOf<Float?>(null) }
     val documentWidth = remember { mutableStateOf<Int?>(null) }
     val (temporaryFile, setTemporaryFile) = remember { mutableStateOf<OkioPlatformMedia.TemporaryFile?>(null) }
     LaunchedEffect(Unit) {
@@ -91,8 +95,7 @@ actual fun PDFReader(
     LaunchedEffect(documentWidth.value, viewSize.width) {
         documentWidth.value?.toFloat()?.let {
             val maxDpi = 1f / it * 1800f
-            val dpi = (viewSize.width / it * scale / density * 2f).coerceAtMost(maxDpi)
-            dpPixel.value = (dpi / it).toInt()
+            dpi.value = (viewSize.width / it * scale / density * 2f).coerceAtMost(maxDpi)
         }
     }
     DisposableEffect(Unit) {
@@ -103,85 +106,73 @@ actual fun PDFReader(
         }
     }
 
-    val readerError = remember { mutableStateOf<String?>(null) }
 
     Box(
         Modifier
             .fillMaxSize()
             .onSizeChanged { viewSize = it }
     ) {
-        if (readerError.value == null) {
-            renderer.value?.let { pdfReader ->
-                if (pdfReader.pageCount == 0) {
-                    CenteredElement {
-                        Text(i18nView.fileOverlayPreviewNotSupported())
-                    }
-                    return@let
-                } else {
-                    val lazyListState = rememberLazyListState()
-                    val horizontalScroll = rememberScrollState()
-                    LazyColumn(
-                        modifier = Modifier
-                            .horizontalScroll(horizontalScroll)
-                            .simpleVerticalScrollbar(lazyListState, MaterialTheme.colorScheme.primary)
-                            .fillMaxWidth(),
-                        verticalArrangement = Arrangement.spacedBy(8.dp),
-                        contentPadding = PaddingValues(8.dp),
-                        state = lazyListState,
-                        content = {
-                            items(count = pdfReader.pageCount, key = { it }) { pageId ->
-                                val cacheKey = "$pageId:${dpPixel.value?.toInt()}"
-                                val currentPage = cache.value[cacheKey]?.second ?: run {
-                                    println("rendering page $pageId")
-                                    pdfReader.openPage(pageId).use { page ->
-                                        val bitmap = Bitmap.createBitmap(
-                                            dpPixel.value?:0,
-                                            page.height,
-                                            Bitmap.Config.ARGB_8888
-                                        )
-                                        page.render(bitmap, null, null, RENDER_MODE_FOR_DISPLAY)
-                                        val image = bitmap.asImageBitmap()
-                                        cache.value[cacheKey] = Pair(System.currentTimeMillis(), image)
-                                        cache.value.toList().sortedBy { it.second.first }
-                                            .subList(0, 0.coerceAtLeast(cache.value.size - pageCacheSize))
-                                            .forEach { cache.value.remove(it.first) }
-                                        image
-                                    }
-                                }
-
-                                Image(
-                                    bitmap = currentPage,
-                                    contentDescription = i18nView.fileOverlayPdfPageDescriptor(pageId),
-                                    modifier = Modifier
-                                        .background(color = Color.White) // Avoid performance drops on transparent images.
-                                        .width(viewSize.width.dp / density * scale - 16.dp),
-                                    contentScale = ContentScale.FillWidth,
+        val pdfReader = renderer.value
+        val dpi = dpi.value
+        if (pdfReader != null && dpi != null && pdfReader.pageCount > 0) {
+            val lazyListState = rememberLazyListState()
+            val horizontalScroll = rememberScrollState()
+            LazyColumn(
+                modifier = Modifier
+                    .transformable(state)
+                    .horizontalScroll(horizontalScroll)
+                    .simpleVerticalScrollbar(lazyListState, MaterialTheme.colorScheme.primary)
+                    .fillMaxWidth(),
+                verticalArrangement = Arrangement.spacedBy(8.dp),
+                contentPadding = PaddingValues(8.dp),
+                state = lazyListState,
+                userScrollEnabled = !isZooming,
+                content = {
+                    items(count = pdfReader.pageCount, key = { it }) { pageId ->
+                        val cacheKey = "$pageId:${dpi}"
+                        val currentPage = cache.value[cacheKey]?.second ?: run {
+                            pdfReader.openPage(pageId).use { page ->
+                                val bitmap = Bitmap.createBitmap(
+                                    (page.width * dpi).toInt(),
+                                    (page.height * dpi).toInt(),
+                                    Bitmap.Config.ARGB_8888
                                 )
+                                page.render(bitmap, null, null, RENDER_MODE_FOR_DISPLAY)
+                                val image = bitmap.asImageBitmap()
+                                cache.value[cacheKey] = Pair(System.currentTimeMillis(), image)
+                                cache.value.toList().sortedBy { it.second.first }
+                                    .subList(0, 0.coerceAtLeast(cache.value.size - pageCacheSize))
+                                    .forEach { cache.value.remove(it.first) }
+                                image
                             }
                         }
-                    )
-                    HorizontalScrollbar(
-                        Modifier
-                            .align(Alignment.BottomCenter)
-                            .fillMaxWidth(),
-                        horizontalScroll,
-                    )
+
+                        Image(
+                            bitmap = currentPage,
+                            contentDescription = i18nView.fileOverlayPdfPageDescriptor(pageId),
+                            modifier = Modifier
+                                .background(color = Color.White) // Avoid performance drops on transparent images.
+                                .width(viewSize.width.dp / density * scale - 16.dp),
+                            contentScale = ContentScale.FillWidth,
+                        )
+                    }
                 }
-            } //No Renderer yet -> Display a loading indicator
-                ?: CenteredElement {
-                    CircularProgressIndicator(Modifier.size(32.dp))
-                }
-        } //Error when reading a file
-        else {
-            val error = readerError.value
-            Column(
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center,
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(32.dp)
-                    .background(Color.Cyan),
-            ) { Text(error.toString()) }
+            )
+            HorizontalScrollbar(
+                Modifier
+                    .align(Alignment.BottomCenter)
+                    .fillMaxWidth(),
+                horizontalScroll,
+            )
+
+        } //No Renderer yet -> Display a loading indicator
+        else if (pdfReader == null) CenteredElement {
+            CircularProgressIndicator(Modifier.size(32.dp))
+        }
+        else if (pdfReader.pageCount > 0) {
+            CenteredElement {
+                Text(i18nView.fileOverlayPreviewNotSupported())
+            }
 
         }
     }
