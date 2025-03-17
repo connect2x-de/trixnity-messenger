853a1b1bcf886c8efdcf83a47f0a3b7b1e5b4c0a
adambrangenberg <adabran06@gmail.com>
2025-03-17 12:00:07 +0100

    Add tests, move joinRoom to its own file

--- trixnity-messenger/src/commonMain/kotlin/de/connect2x/trixnity/messenger/DI.kt
+++ trixnity-messenger/src/commonMain/kotlin/de/connect2x/trixnity/messenger/DI.kt
@@ -18,7 +18,7 @@ import de.connect2x.trixnity.messenger.util.SearchImpl
 import de.connect2x.trixnity.messenger.util.SharedDataHandler
 import de.connect2x.trixnity.messenger.util.SharedDataHandlerImpl
 import de.connect2x.trixnity.messenger.util.convertSecretByteArrayModule
-import de.connect2x.trixnity.messenger.util.joinRoom
+import de.connect2x.trixnity.messenger.util.JoinRoom
 import de.connect2x.trixnity.messenger.util.platformCloseAppModule
 import de.connect2x.trixnity.messenger.util.platformDeleteAccountDataModule
 import de.connect2x.trixnity.messenger.util.platformGetDefaultDisplayNameModule
@@ -231,7 +231,7 @@ fun createTrixnityMessengerDefaultModuleFactories(): List<ModuleFactory> = listO
             single<RoomTopic> { RoomTopicImpl() }
             single<RoomInviter> { RoomInviterImpl() }
             single<UserBlocking> { UserBlockingImpl() }
-            single<joinRoom> { joinRoom }
+            single<JoinRoom> { JoinRoom }
 
             single<DownloadManager> { DownloadManagerImpl() }
             single<Thumbnails> { ThumbnailsImpl() }
