5da77b82470ea3068b4c9eba1803609b8bea33e6
adambrangenberg <adabran06@gmail.com>
2025-03-20 22:56:46 +0100

    Explicitly matching and testing JoinRule.isKnock

---
+++ trixnity-messenger/src/commonTest/kotlin/de/connect2x/trixnity/messenger/util/JoinRuleTest.kt
@@ -0,0 +1,19 @@
+package de.connect2x.trixnity.messenger.util
+
+import io.kotest.matchers.shouldBe
+import net.folivo.trixnity.core.model.events.m.room.JoinRulesEventContent
+import kotlin.test.Test
+
+class JoinRuleTest {
+    @Test
+    fun `isKnock - should return true for knocks and false for everything else`() {
+        JoinRulesEventContent.JoinRule.Knock.isKnock shouldBe true
+        JoinRulesEventContent.JoinRule.KnockRestricted.isKnock shouldBe true
+
+        JoinRulesEventContent.JoinRule.Invite.isKnock shouldBe false
+        JoinRulesEventContent.JoinRule.Private.isKnock shouldBe false
+        JoinRulesEventContent.JoinRule.Public.isKnock shouldBe false
+        JoinRulesEventContent.JoinRule.Restricted.isKnock shouldBe false
+        JoinRulesEventContent.JoinRule.Unknown("").isKnock shouldBe false
+    }
+}
