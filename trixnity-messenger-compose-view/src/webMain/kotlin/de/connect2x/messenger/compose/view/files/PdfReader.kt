fa8559834c4cce4123eef057e2a2ef270c0f8ffa
jlddorf <jakob.deutschendorf@gmail.com>
2025-03-24 10:40:06 +0100

    update bitmaps inside cache map values

--- trixnity-messenger-compose-view/src/webMain/kotlin/de/connect2x/messenger/compose/view/files/PdfReader.kt
+++ trixnity-messenger-compose-view/src/webMain/kotlin/de/connect2x/messenger/compose/view/files/PdfReader.kt
@@ -2,6 +2,7 @@ package de.connect2x.messenger.compose.view.files
 
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.graphics.ImageBitmap
+import io.github.oshai.kotlinlogging.KotlinLogging
 import js.date.Date
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -13,6 +14,7 @@ import web.url.URL
 import kotlin.math.max
 import kotlin.math.min
 
+private val log = KotlinLogging.logger { }
 
 class PdfReaderWeb(blob: Blob) {
     init {
@@ -20,13 +22,14 @@ class PdfReaderWeb(blob: Blob) {
         jsReader.promise.then { loadedDocument ->
             pdfDocument.value = loadedDocument
             pageSize.value = loadedDocument.numPages
+            loadedDocument.getPage(1).then { documentWidth.value = it.getViewport(ViewportParams(1f)).width }
         }
     }
 
     private val pageCacheSize = max(2f, min(16f, 8f / 1f)).toInt()
 
-    val scale = mutableStateOf(1f)
-    val cache = mutableMapOf<String, Pair<Long, MutableStateFlow<ImageBitmap?>>>()
+    val documentWidth = mutableStateOf<Int?>(null)
+    val cache = mutableMapOf<String, Triple<Long, MutableStateFlow<ImageBitmap?>, Int>>()
 
     private val pdfDocument = mutableStateOf<PDFDocumentProxy?>(null)
     private val dom = document
@@ -38,29 +41,36 @@ class PdfReaderWeb(blob: Blob) {
         override var viewport: PDFPageViewport
     ) : PDFRenderParams
 
-    fun renderPage(pageIndex: Int, urlFlow: MutableStateFlow<ImageBitmap?>) {
+    fun renderPage(pageIndex: Int, urlFlow: MutableStateFlow<ImageBitmap?>, scale: Float) {
         pageSize.value?.let { pageSize ->
             if (pageIndex <= pageSize) {
                 val canvas = getOrCreatePageCanvas(pageIndex)
                 val document = pdfDocument.value
                 document?.getPage(pageIndex)?.then { page ->
-                    val viewport = page.getViewport(params = ViewportParams(scale.value))
+                    val pageScale = scale / 72f
+                    val scaledViewport = page.getViewport(params = ViewportParams(scale = pageScale))
+                    log.debug {
+                        "render pdf page $pageIndex " +
+                                "to viewport (${scaledViewport.width}x${scaledViewport.height}) " +
+                                "at scale factor: $scale " +
+                                "with ${cache.size} pages already cached"
+                    }
+                    canvas.height = scaledViewport.height
+                    canvas.width = scaledViewport.width
                     val context = canvas.getContext(CanvasRenderingContext2D.ID)
-                    canvas.height = viewport.height
-                    canvas.width = viewport.width
                     context?.let {
-                        page.render(RenderParams(context, viewport)).promise.then {
+                        page.render(RenderParams(context, scaledViewport)).promise.then {
                             canvas.toBlob(callback = { blob ->
                                 blob?.let {
                                     blob.bytesAsync().then {
                                         urlFlow.value = it.toByteArray().toImageBitmap()
-                                        println("Created bitmap for $pageIndex")
+                                        dom.getElementById("pdf-canvas-page-$pageIndex")?.remove()
                                     }
                                 }
                             })
                         }
                     }
-                } ?: throw IllegalStateException("Pages with index $pageIndex doesn't exist")
+                } ?: throw IllegalStateException("Page with index $pageIndex doesn't exist")
             } else throw IllegalArgumentException("Page index must be smaller or equal to Page size ($pageSize)")
         } ?: throw IllegalStateException("Pages haven't been loaded yet")
     }
@@ -72,19 +82,25 @@ class PdfReaderWeb(blob: Blob) {
             }) as HTMLCanvasElement
     }
 
-    fun getOrRenderPage(pageId: Int): StateFlow<ImageBitmap?> {
-        println("Starting render of $pageId")
+    fun getOrRenderPage(pageId: Int, scale: Float): StateFlow<ImageBitmap?> {
         val oneBasedId = pageId + 1
         val cacheKey = "$oneBasedId"
-        return cache[cacheKey]?.second ?: run {
-            val urlFlow = MutableStateFlow<ImageBitmap?>(null)
-            cache[cacheKey] = Pair(Date.now().toLong(), urlFlow)
+        val element = cache[cacheKey]
+        return if (element?.third == scale.toInt()) {
+            element.second
+        } else {
+            val urlFlow = element?.second ?: MutableStateFlow<ImageBitmap?>(null)
+            cache[cacheKey] = Triple(Date.now().toLong(), urlFlow, scale.toInt())
             urlFlow.also {
-                renderPage(oneBasedId, urlFlow)
+                renderPage(oneBasedId, urlFlow, scale)
                 cache.toList().sortedBy { it.second.first }
                     .subList(0, 0.coerceAtLeast(cache.size - pageCacheSize))
                     .forEach { cache.remove(it.first) }
             }
         }
+    }
+
+    fun clearCache() {
+        cache.clear()
     }
 }
