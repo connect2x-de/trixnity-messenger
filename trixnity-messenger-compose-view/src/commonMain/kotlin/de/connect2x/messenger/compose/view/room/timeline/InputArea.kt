6adbea38ac2f6b4635c12e1cb534a7eaaccd8409
Janne Mareike Koschinski <mail@justjanne.de>
2025-03-24 05:45:21 +0100

    Fix unnecessary input area clipping

--- trixnity-messenger-compose-view/src/commonMain/kotlin/de/connect2x/messenger/compose/view/room/timeline/InputArea.kt
+++ trixnity-messenger-compose-view/src/commonMain/kotlin/de/connect2x/messenger/compose/view/room/timeline/InputArea.kt
@@ -46,7 +46,6 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.SolidColor
@@ -265,7 +264,6 @@ fun RowScope.InputAreaTextField(
             modifier = Modifier
                 .focusRequester(focusRequester)
                 .fillMaxWidth()
-                .clip(style.shape)
                 .onPreviewKeyEvent {
                     if (it.type == KeyEventType.KeyDown) {
                         when {
