d9b7091f954d047d16bc675a4d8bbc615dae70b3
Felix Hilgers <contact@fhilgers.com>
2025-03-25 21:53:07 +0000

    fix emojis on ios

--- trixnity-messenger/src/commonMain/kotlin/de/connect2x/trixnity/messenger/util/Strings.kt
+++ trixnity-messenger/src/commonMain/kotlin/de/connect2x/trixnity/messenger/util/Strings.kt
@@ -9,12 +9,12 @@ import org.koin.dsl.module
  * https://unicode.org/emoji/charts/full-emoji-modifiers.html
  * for the ranges.
  */
-private const val SURROGATE_PAIR: String = """[\uD83C-\uDBFF\uDC00-\uDFFF]+"""
-private const val SYMBOLICS: String = """[\u203C-\u3299]|[\u00A9\u00AE\u2122\u3030]"""
-private const val KEYCAPS: String = """[\u0023-\u0039]\uFE0F?\u20E3"""
-private const val VAR_SELECTOR: String = """\uFE0F"""
-private const val FLAGS: String = """(?:\uD83C[\uDDE6-\uDDFF]){2}"""
-private const val DIACRITICS: String = """[\u0300-\u036F]"""
+private const val SURROGATE_PAIR: String = "[\uD83C-\uDBFF\uDC00-\uDFFF]+"
+private const val SYMBOLICS: String = "[\u203C-\u3299]|[\u00A9\u00AE\u2122\u3030]"
+private const val KEYCAPS: String = "[\u0023-\u0039]\uFE0F?\u20E3"
+private const val VAR_SELECTOR: String = "\uFE0F"
+private const val FLAGS: String = "(?:\uD83C[\uDDE6-\uDDFF]){2}"
+private const val DIACRITICS: String = "[\u0300-\u036F]"
 
 private val emojiPattern: Regex =
     Regex("^($SURROGATE_PAIR|$SYMBOLICS|$KEYCAPS|$FLAGS|$DIACRITICS|$VAR_SELECTOR)+$")
