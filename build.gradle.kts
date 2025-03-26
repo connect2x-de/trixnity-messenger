12d6535fda941a19137f0857cfb34b61e50a1238
KitsuneAlex <alexh@karmakrafts.dev>
2025-03-26 19:32:32 +0100

    Refactor root buildscript

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,53 +1,26 @@
-buildscript {
-    repositories {
-        mavenLocal()
-        google()
-        mavenCentral()
-    }
-
-    dependencies {
-        classpath("com.android.tools.build:gradle:${libs.versions.androidGradle.get()}")
-        classpath(kotlin("gradle-plugin", version = libs.versions.kotlin.get()))
-        // ui
-        classpath(libs.gms.google.services)
-    }
-}
-
 plugins {
-    alias(libs.plugins.kotest).apply(false)
-    kotlin("plugin.serialization") version libs.versions.kotlin.get() apply false
-    alias(libs.plugins.mokkery).apply(false)
-    alias(libs.plugins.skie).apply(false)
-    alias(libs.plugins.kmmbridge).apply(false)
-    `maven-publish`
+    alias(libs.plugins.kotlin.multiplatform) apply false
+    alias(libs.plugins.kotlin.serialization) apply false
+    alias(libs.plugins.kotlinx.kover) apply false
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotest) apply false
+    alias(libs.plugins.mokkery) apply false
+    alias(libs.plugins.skie) apply false
+    alias(libs.plugins.kmmbridge) apply false
+    alias(libs.plugins.compose.multiplatform) apply false
     alias(libs.plugins.dokka)
-    alias(libs.plugins.kotlinx.kover)
-
-    // ui
-    alias(libs.plugins.compose.multiplatform).apply(false)
-    alias(libs.plugins.compose.compiler).apply(false)
+    `maven-publish`
 }
 
 allprojects {
     group = "de.connect2x"
     version = withVersionSuffix("3.5.1")
-
-    repositories {
-        mavenCentral()
-        google()
-        mavenLocal()
-        maven("https://oss.sonatype.org/content/repositories/snapshots")
-        maven("https://gitlab.com/api/v4/projects/26519650/packages/maven") // trixnity
-        maven("https://gitlab.com/api/v4/projects/58749664/packages/maven") // sysnotify
-        maven("https://gitlab.com/api/v4/projects/65998892/packages/maven") // androidx
-    }
-
     if (System.getenv("WITH_LOCK")?.toBoolean() == true) {
         dependencyLocking {
             lockAllConfigurations()
         }
-
-        val dependenciesForAll by tasks.registering(DependencyReportTask::class) {}
+        val dependenciesForAll by tasks.registering(DependencyReportTask::class)
     }
 }
 
@@ -59,13 +32,15 @@ subprojects {
         val dokkaJar by tasks.registering(Jar::class) {
             dependsOn("dokkaGenerate")
             from(dokka.dokkaPublications.html.flatMap { it.outputDirectory })
-            archiveClassifier.set("javadoc")
+            archiveClassifier = "javadoc"
             onlyIf { isCI }
         }
+
         publishing {
             repositories {
                 maven {
-                    url = uri("${System.getenv("CI_API_V4_URL")}/projects/47538655/packages/maven")
+                    url =
+                        uri("${System.getenv("CI_API_V4_URL")}/projects/${System.getenv("CI_PROJECT_ID")}/packages/maven")
                     name = "GitLab"
                     credentials(HttpHeaderCredentials::class) {
                         name = "Job-Token"
@@ -79,23 +54,39 @@ subprojects {
             publications.configureEach {
                 if (this is MavenPublication) {
                     pom {
-                        name.set(project.name)
-                        description.set("Multiplatform Kotlin SDK for Matrix messengers")
-                        url.set("https://gitlab.com/connect2x/trixnity-messenger/trixnity-messenger")
+                        name = project.name
+                        description = "Multiplatform Kotlin SDK for Matrix messengers"
+                        url = "https://gitlab.com/connect2x/trixnity-messenger/trixnity-messenger"
                         licenses {
                             license {
-                                name.set("GNU AFFERO GENERAL PUBLIC LICENSE version 3")
-                                url.set("https://www.gnu.org/licenses/agpl-3.0.html")
+                                name = "GNU AFFERO GENERAL PUBLIC LICENSE version 3"
+                                url = "https://www.gnu.org/licenses/agpl-3.0.html"
                             }
                         }
                         developers {
                             developer {
-                                id.set("michael.thiele")
-                                id.set("benkuly")
+                                id = "benkuly"
+                                name = "Benedict Benken"
+                            }
+                            developer {
+                                id = "michael.thiele"
+                                name = "Michael Thiele"
+                            }
+                            developer {
+                                id = "janne.koschinski"
+                                name = "Janne Koschinski"
+                            }
+                            developer {
+                                id = "KitsuneAlex"
+                                name = "Alexander Hinze"
+                            }
+                            developer {
+                                id = "cach30verfl0w"
+                                name = "Cedric Hammes"
                             }
                         }
                         scm {
-                            url.set("https://gitlab.com/connect2x/trixnity-messenger/trixnity-messenger")
+                            url = this@pom.url
                         }
                     }
                     if (isCI) artifact(dokkaJar)
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,5 @@
 rootProject.name = "trixnity-messenger-root"
+
 include(
     "trixnity-messenger",
     "integrationtests",
@@ -10,7 +11,28 @@ include(
 buildCache {
     local {
         directory = File(rootDir, ".gradle").resolve("build-cache")
-        removeUnusedEntriesAfterDays = 30 // TODO: deprecated
+    }
+}
+
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        mavenLocal()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    // Suppress is okay because it is an incubating API, the suppression name just doesn't match
+    @Suppress("UnstableApiUsage") repositories {
+        mavenCentral()
+        google()
+        mavenLocal()
+        maven("https://oss.sonatype.org/content/repositories/snapshots")
+        maven("https://gitlab.com/api/v4/projects/26519650/packages/maven") // trixnity
+        maven("https://gitlab.com/api/v4/projects/58749664/packages/maven") // sysnotify
+        maven("https://gitlab.com/api/v4/projects/65998892/packages/maven") // androidx
     }
 }
 
