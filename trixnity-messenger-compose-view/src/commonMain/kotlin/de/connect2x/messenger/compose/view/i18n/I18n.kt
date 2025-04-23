153bc438b9c27be0ccc5b0374560bae920fd2902
Janne Mareike Koschinski <mail@justjanne.de>
2025-04-23 13:18:54 +0200

    Remove UI again

--- trixnity-messenger-compose-view/src/commonMain/kotlin/de/connect2x/messenger/compose/view/i18n/I18n.kt
+++ trixnity-messenger-compose-view/src/commonMain/kotlin/de/connect2x/messenger/compose/view/i18n/I18n.kt
@@ -2812,11 +2812,6 @@ open class I18nView(
         EN - "Why do you wish to join? (Optional)"
         DE - "Warum möchtest du beitreten? (Optional)"
     }
-
-    open fun deactivateAccount() = translate {
-        EN - "Deactivate Account"
-        DE - "Account deaktivieren"
-    }
 }
 
 fun i18nViewModule() = module {
