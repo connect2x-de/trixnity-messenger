c9cba10a8d0ea06876d012b0b41d64d0d633672d
cach30verfl0w <cach30verfl0w@gmail.com>
2025-06-24 10:38:10 +0200

    Add KDoc to isSupportedImage

--- trixnity-messenger/src/commonMain/kotlin/de/connect2x/trixnity/messenger/util/SupportedMimeTypes.kt
+++ trixnity-messenger/src/commonMain/kotlin/de/connect2x/trixnity/messenger/util/SupportedMimeTypes.kt
@@ -8,9 +8,14 @@ object SupportedMimeTypes {
         ContentType.Image.PNG,
         ContentType.Image.BMP,
         ContentType.Image.Webp,
-        ContentType.Image.GIF // gifs can be rendered statically (first frame)
+        ContentType.Image.GIF
     )
 
-    // JPEG, PNG, BMP, WEBP (based on decodeToImageBitmap())
+    /**
+     * Checks if the provided MIME type is a supported image format.
+     *
+     * Supported formats are JPEG, PNG, BMP, WebP, and GIF. Note that `decodeToImageBitmap` may only fully support
+     * JPEG, PNG, BMP, and GIF (statically via the first frame).
+     */
     fun isSupportedImage(mimeType: ContentType): Boolean = supportedImageMimeTypes.any { it.match(mimeType) }
 }
