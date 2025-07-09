c660e315683f28812427b6a44ddeb46f31654312
cach30verfl0w <cach30verfl0w@gmail.com>
2025-07-09 12:13:25 +0200

    Add lifecycle pause and resume for iOS app

--- trixnity-messenger-compose-app/src/iosMain/kotlin/de/connect2x/messenger/MainController.kt
+++ trixnity-messenger-compose-app/src/iosMain/kotlin/de/connect2x/messenger/MainController.kt
@@ -1,11 +1,18 @@
 package de.connect2x.messenger
 
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.window.ComposeUIViewController
 import com.arkivanov.decompose.DefaultComponentContext
 import com.arkivanov.essenty.lifecycle.Lifecycle
+import com.arkivanov.essenty.lifecycle.LifecycleRegistry
+import com.arkivanov.essenty.lifecycle.pause
+import com.arkivanov.essenty.lifecycle.resume
+import com.arkivanov.essenty.lifecycle.stop
 import de.connect2x.messenger.compose.view.Client
 import de.connect2x.messenger.compose.view.DI
 import de.connect2x.messenger.compose.view.IsFocused
@@ -18,12 +25,16 @@ import de.connect2x.messenger.compose.view.theme.MessengerTheme
 import de.connect2x.trixnity.messenger.multi.MatrixMultiMessenger
 import de.connect2x.trixnity.messenger.multi.create
 import io.github.oshai.kotlinlogging.KotlinLogging
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.runBlocking
+import platform.Foundation.NSNotificationCenter
+import platform.UIKit.UIApplicationDidEnterBackgroundNotification
+import platform.UIKit.UIApplicationWillEnterForegroundNotification
 import platform.UIKit.UIViewController
 
 private val log = KotlinLogging.logger {}
 
-fun MainViewController(lifecycle: Lifecycle): UIViewController {
+fun MainViewController(lifecycle: LifecycleRegistry): UIViewController {
     log.info { "Starting iOS client" }
     val matrixMultiMessenger = runBlocking {
         MatrixMultiMessenger.create(
@@ -35,16 +46,26 @@ fun MainViewController(lifecycle: Lifecycle): UIViewController {
     return ComposeUIViewController(
         configure = { enforceStrictPlistSanityCheck = false }
     ) {
+        var isFocused by remember { mutableStateOf(false) }
+
         WithProfileSelection(
             matrixMultiMessenger,
             componentContext = DefaultComponentContext(lifecycle),
             activeMessengerOnce = { _, _ ->
-                // TODO
+                val notificationCenter = NSNotificationCenter.defaultCenter()
+                notificationCenter.addObserverForName(UIApplicationDidEnterBackgroundNotification, null, null) { _ ->
+                    isFocused = false
+                    lifecycle.pause()
+                }
+                notificationCenter.addObserverForName(UIApplicationWillEnterForegroundNotification, null, null) { _ ->
+                    isFocused = true
+                    lifecycle.resume()
+                }
             },
             activeMessenger = { matrixMessenger, rootViewModel ->
                 CompositionLocalProvider(
-                    Platform provides PlatformType.IOS, // TODO iOS
-                    IsFocused provides true, // TODO
+                    Platform provides PlatformType.IOS,
+                    IsFocused provides isFocused,
                     DI provides matrixMessenger.di,
                 ) {
                     MessengerTheme {
@@ -55,8 +76,8 @@ fun MainViewController(lifecycle: Lifecycle): UIViewController {
             nonActiveMessenger = { existingProfiles ->
                 val showProfileCreation = remember { mutableStateOf(false) }
                 CompositionLocalProvider(
-                    Platform provides PlatformType.IOS, // TODO
-                    IsFocused provides true, // TODO
+                    Platform provides PlatformType.IOS,
+                    IsFocused provides isFocused,
                     DI provides matrixMultiMessenger.di,
                     ShowProfileCreation provides showProfileCreation,
                 ) {
