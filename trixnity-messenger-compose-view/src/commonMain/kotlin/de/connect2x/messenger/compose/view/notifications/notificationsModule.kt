b07d500759536fa21b0c3048ba5b321c7eb7c047
KitsuneAlex <alexh@karmakrafts.dev>
2025-07-11 15:46:45 +0200

    Add warning to noopNotificationsModule

--- trixnity-messenger-compose-view/src/commonMain/kotlin/de/connect2x/messenger/compose/view/notifications/notificationsModule.kt
+++ trixnity-messenger-compose-view/src/commonMain/kotlin/de/connect2x/messenger/compose/view/notifications/notificationsModule.kt
@@ -3,8 +3,12 @@ package de.connect2x.messenger.compose.view.notifications
 import de.connect2x.sysnotify.NoopNotificationHandler
 import de.connect2x.sysnotify.NotificationHandler
 import de.connect2x.trixnity.messenger.MatrixMessengerBaseConfiguration
+import io.github.oshai.kotlinlogging.KLogger
+import io.github.oshai.kotlinlogging.KotlinLogging
 import org.koin.core.module.Module
 import org.koin.dsl.module
+
+private val log: KLogger = KotlinLogging.logger {}
 
 fun notificationsModule(
     config: MatrixMessengerBaseConfiguration,
@@ -24,6 +28,7 @@ fun notificationsModule(
 
 fun noopNotificationsModule(): Module = module {
     single<NotificationHandlerProvider> {
+        log.warn { "You are currently using the noopNotificationsModule and will receive no notifications" }
         NotificationHandlerProvider.of(NoopNotificationHandler)
     }
 }
