007dd79b3a2a096c2cf52ddb30a0105d2b67effd
KitsuneAlex <alexh@karmakrafts.dev>
2025-07-11 20:21:54 +0200

    Properly dispose NotificationHandlers when messenger insstance is closed

--- trixnity-messenger-compose-view/src/commonMain/kotlin/de/connect2x/messenger/compose/view/notifications/NotificationHandlerProvider.kt
+++ trixnity-messenger-compose-view/src/commonMain/kotlin/de/connect2x/messenger/compose/view/notifications/NotificationHandlerProvider.kt
@@ -1,27 +1,34 @@
 package de.connect2x.messenger.compose.view.notifications
 
+import co.touchlab.stately.collections.ConcurrentMutableMap
 import de.connect2x.sysnotify.NotificationHandler
 
-fun interface NotificationHandlerProvider {
+interface NotificationHandlerProvider {
     companion object {
         const val GLOBAL: String = "global" // The subId of the global NotificationHandler instance
 
         inline fun lazy(crossinline block: (String) -> NotificationHandler): NotificationHandlerProvider {
             return object : NotificationHandlerProvider {
-                private var instance: NotificationHandler? = null
+                private val instances: ConcurrentMutableMap<String, NotificationHandler> = ConcurrentMutableMap()
                 override fun invoke(subId: String): NotificationHandler {
-                    if (instance == null) {
-                        instance = block(subId)
-                    }
-                    return requireNotNull(instance) { "Could not create NotificationHandler for sub-ID '$subId'" }
+                    return instances.getOrPut(subId) { block(subId) }
+                }
+
+                override fun closeAll() {
+                    instances.values.forEach(NotificationHandler::close)
                 }
             }
         }
 
         fun of(handler: NotificationHandler): NotificationHandlerProvider {
-            return NotificationHandlerProvider { handler }
+            return object : NotificationHandlerProvider {
+                override fun invoke(subId: String): NotificationHandler = handler
+                override fun closeAll() = handler.close()
+            }
         }
     }
 
     operator fun invoke(subId: String): NotificationHandler
+
+    fun closeAll()
 }
--- trixnity-messenger-compose-view/src/commonMain/kotlin/de/connect2x/messenger/compose/view/notifications/notificationsModule.kt
+++ trixnity-messenger-compose-view/src/commonMain/kotlin/de/connect2x/messenger/compose/view/notifications/notificationsModule.kt
@@ -3,6 +3,10 @@ package de.connect2x.messenger.compose.view.notifications
 import de.connect2x.sysnotify.NoopNotificationHandler
 import de.connect2x.sysnotify.NotificationHandler
 import de.connect2x.trixnity.messenger.MatrixMessengerBaseConfiguration
+import de.connect2x.trixnity.messenger.MatrixMessengerCloseHook
+import de.connect2x.trixnity.messenger.MatrixMessengerConfiguration
+import de.connect2x.trixnity.messenger.multi.MatrixMultiMessengerCloseHook
+import de.connect2x.trixnity.messenger.multi.MatrixMultiMessengerConfiguration
 import io.github.oshai.kotlinlogging.KLogger
 import io.github.oshai.kotlinlogging.KotlinLogging
 import org.koin.core.module.Module
@@ -14,21 +18,30 @@ fun notificationsModule(
     config: MatrixMessengerBaseConfiguration,
     isDebugEnabled: Boolean
 ): Module = module {
-    single<NotificationHandlerProvider> {
-        NotificationHandlerProvider.lazy { subId ->
-            NotificationHandler(
-                name = config.appName,
-                id = "$it.$subId",
-                appId = config.appId,
-                isDebugEnabled = isDebugEnabled
-            )
+    val provider = NotificationHandlerProvider.lazy { subId ->
+        NotificationHandler(
+            name = config.appName,
+            id = "${config.appId}.$subId",
+            appId = config.appId,
+            isDebugEnabled = isDebugEnabled
+        )
+    }
+    single<NotificationHandlerProvider> { provider }
+    when (config) {
+        is MatrixMessengerConfiguration -> single<MatrixMessengerCloseHook> {
+            MatrixMessengerCloseHook { provider.closeAll() }
+        }
+
+        is MatrixMultiMessengerConfiguration -> single<MatrixMultiMessengerCloseHook> {
+            MatrixMultiMessengerCloseHook { provider.closeAll() }
         }
     }
 }
 
 fun noopNotificationsModule(): Module = module {
+    log.warn { "You are currently using the noopNotificationsModule and will receive no notifications" }
     single<NotificationHandlerProvider> {
-        log.warn { "You are currently using the noopNotificationsModule and will receive no notifications" }
         NotificationHandlerProvider.of(NoopNotificationHandler)
     }
+    // No need for a close hook here because NoopNotificationHandler never allocates resources
 }
