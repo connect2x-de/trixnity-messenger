7410112880e56fb7781cf7fc7dbd1515f79b4fdf
cach30verfl0w <cach30verfl0w@gmail.com>
2025-07-28 14:43:39 +0200

    Add dialogs on iOS targets

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -9,6 +9,8 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ### Added
 
+- File Load and Save dialogs on iOS targets
+
 ### Changed
 
 - New rich text renderer
--- trixnity-messenger-compose-view/src/iosMain/kotlin/de/connect2x/messenger/compose/view/files/Dialogs.ios.kt
+++ trixnity-messenger-compose-view/src/iosMain/kotlin/de/connect2x/messenger/compose/view/files/Dialogs.ios.kt
@@ -1,9 +1,34 @@
 package de.connect2x.messenger.compose.view.files
 
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import de.connect2x.messenger.compose.view.DI
 import de.connect2x.messenger.compose.view.common.FilePickerType
+import de.connect2x.messenger.compose.view.get
+import de.connect2x.messenger.compose.view.i18n.I18nView
+import de.connect2x.messenger.compose.view.theme.components
+import de.connect2x.messenger.compose.view.theme.components.ModalDialogContent
+import de.connect2x.messenger.compose.view.theme.components.ModalDialogFooter
+import de.connect2x.messenger.compose.view.theme.components.ModalDialogHeader
+import de.connect2x.messenger.compose.view.theme.components.ThemedButton
+import de.connect2x.messenger.compose.view.theme.components.ThemedModalDialog
 import de.connect2x.trixnity.messenger.util.FileDescriptor
+import de.connect2x.trixnity.messenger.util.PathFileDescriptor
+import io.github.oshai.kotlinlogging.KotlinLogging
+import io.github.vinceglb.filekit.FileKit
+import io.github.vinceglb.filekit.dialogs.FileKitMode
+import io.github.vinceglb.filekit.dialogs.FileKitType
+import io.github.vinceglb.filekit.dialogs.compose.rememberFilePickerLauncher
+import io.github.vinceglb.filekit.dialogs.openFileSaver
+import io.github.vinceglb.filekit.path
 import net.folivo.trixnity.client.media.PlatformMedia
+import net.folivo.trixnity.utils.write
+import okio.FileSystem
+import okio.Path.Companion.toPath
+
+private val log = KotlinLogging.logger {}
 
 @Composable
 actual fun SaveFileDialog(
@@ -13,7 +38,42 @@ actual fun SaveFileDialog(
     downloadFile: (suspend (PlatformMedia) -> Unit) -> Unit,
     onCloseSaveFileDialog: () -> Unit,
 ) {
-    // TODO
+    val i18n = DI.get<I18nView>()
+    val hasError = error?.isNotBlank() == true
+    if (hasError) {
+        ThemedModalDialog(onCloseSaveFileDialog) {
+            ModalDialogHeader {
+                Text(i18n.fileDialogDownloadErrorSave())
+            }
+            ModalDialogContent {
+                Text(error)
+            }
+            ModalDialogFooter {
+                ThemedButton(
+                    style = MaterialTheme.components.primaryButton,
+                    onClick = onCloseSaveFileDialog,
+                ) {
+                    Text(i18n.actionOk())
+                }
+            }
+        }
+    }
+    LaunchedEffect(hasError) {
+        if (!hasError) downloadFile {
+            val file = FileKit.openFileSaver(
+                suggestedName = fileName.substringBeforeLast("."),
+                extension = fileName.substringAfterLast("."),
+                // TODO: set initialDirectory to OS dependent default pictures directory
+            )
+            try {
+                val path = file?.path?.toPath()
+                if (path != null) FileSystem.SYSTEM.write(path, it)
+                else log.warn { "no valid path selected" }
+            } finally {
+                onCloseSaveFileDialog()
+            }
+        }
+    }
 }
 
 @Composable
@@ -22,7 +82,25 @@ actual fun LoadFileDialog(
     onFileSelect: (FileDescriptor) -> Unit,
     onCloseLoadFileDialog: () -> Unit,
 ) {
-    // TODO
+    val i18n = DI.get<I18nView>()
+    val fileSystem = DI.get<FileSystem>()
+    val launcher = rememberFilePickerLauncher(
+        type = when {
+            availableTypes.size == 1 && availableTypes.first() == FilePickerType.IMAGE_FILE -> FileKitType.Image
+            availableTypes.size == 1 && availableTypes.first() == FilePickerType.IMAGE_AND_VIDEO_FILE -> FileKitType.ImageAndVideo
+            else -> FileKitType.File()
+        },
+        mode = FileKitMode.Single,
+        title = i18n.fileDialogTitleLoad()
+    ) { file ->
+        file?.let {
+            onFileSelect(PathFileDescriptor(file.path.toPath(), fileSystem))
+        }
+        onCloseLoadFileDialog()
+    }
+    LaunchedEffect(Unit) { // To be safe, wrap the `launch` call.
+        launcher.launch()
+    }
 }
 
 actual fun filterFilePickerOptionsByAvailability(
