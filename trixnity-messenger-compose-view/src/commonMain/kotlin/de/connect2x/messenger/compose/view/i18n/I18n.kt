76c0b12f56459c2d8b3f506449e13512d5d165c6
benkuly <kontakt@folivo.net>
2025-08-04 15:19:01 +0200

    i18n update

--- trixnity-messenger-compose-view/src/commonMain/kotlin/de/connect2x/messenger/compose/view/i18n/I18n.kt
+++ trixnity-messenger-compose-view/src/commonMain/kotlin/de/connect2x/messenger/compose/view/i18n/I18n.kt
@@ -877,7 +877,7 @@ open class I18nView(
     }
 
     open fun userProfileNoteText() = translate {
-        EN - """Every user in a chat/group is given a value between 0 and 100.
+        EN - """Every user in a chat/group has a given power level (number).
             |This value determines the rights a user has in this chat or group.
             |
             |The higher the value, the more rights a user has.
@@ -885,7 +885,7 @@ open class I18nView(
             |
             |Standard values are: 0 (user), 50 (moderator) and 100 (administrator).
         """.trimMargin()
-        DE - """Jedem Nutzer in einem Chat/Gruppe wird ein Wert zwischen 0 und 100 zugewiesen.
+        DE - """Jedem Nutzer in einem Chat/Gruppe hat ein Berechtigungslevel (Zahl).
             |Dieser Wert bestimmt welche Rechte ein Nutzer in diesem Chat oder dieser Gruppe besitzt.
             |
             |Je höher dieser Wert ist, desto mehr Rechte besitzt der Nutzer.
