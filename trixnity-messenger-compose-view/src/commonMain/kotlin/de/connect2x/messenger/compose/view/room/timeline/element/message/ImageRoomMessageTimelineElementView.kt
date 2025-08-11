c7ffc372047cd7240a6dbe09eb12d2f03ceeaf18
Janne Mareike Koschinski <mail@justjanne.de>
2025-08-11 11:15:12 +0200

    Add blurhash

--- trixnity-messenger-compose-view/src/commonMain/kotlin/de/connect2x/messenger/compose/view/room/timeline/element/message/ImageRoomMessageTimelineElementView.kt
+++ trixnity-messenger-compose-view/src/commonMain/kotlin/de/connect2x/messenger/compose/view/room/timeline/element/message/ImageRoomMessageTimelineElementView.kt
@@ -1,15 +1,22 @@
 package de.connect2x.messenger.compose.view.room.timeline.element.message
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredHeightIn
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Image
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -19,27 +26,33 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.ImageBitmap
-import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.graphics.painter.BitmapPainter
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 import de.connect2x.messenger.compose.view.DI
-import de.connect2x.messenger.compose.view.buttonPointerModifier
 import de.connect2x.messenger.compose.view.common.FileName
 import de.connect2x.messenger.compose.view.common.LoadingSpinner
+import de.connect2x.messenger.compose.view.common.customClickable
 import de.connect2x.messenger.compose.view.files.toImageBitmap
 import de.connect2x.messenger.compose.view.get
 import de.connect2x.messenger.compose.view.i18n.I18nView
 import de.connect2x.messenger.compose.view.room.timeline.element.TimelineElementView
 import de.connect2x.messenger.compose.view.room.timeline.element.message.bubble.ReferencedMessagePill
 import de.connect2x.messenger.compose.view.room.timeline.element.util.shortenFileName
+import de.connect2x.messenger.compose.view.theme.components
+import de.connect2x.messenger.compose.view.theme.components.ThemedProgressIndicator
+import de.connect2x.messenger.compose.view.theme.components.ThemedSurface
 import de.connect2x.messenger.compose.view.theme.dp
 import de.connect2x.messenger.compose.view.theme.messengerColors
 import de.connect2x.messenger.compose.view.theme.messengerIcons
+import de.connect2x.messenger.compose.view.util.BlurHashDecoder
+import de.connect2x.messenger.compose.view.util.animateImage
 import de.connect2x.trixnity.messenger.viewmodel.room.timeline.elements.BaseTimelineElementHolderViewModel
 import de.connect2x.trixnity.messenger.viewmodel.room.timeline.elements.TimelineElementHolderViewModel
 import de.connect2x.trixnity.messenger.viewmodel.room.timeline.elements.message.RoomMessageTimelineElementViewModel.FileBased.Image
+import net.folivo.trixnity.core.MSC2448
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 import kotlin.reflect.KClass
 
@@ -128,86 +141,62 @@ internal fun ColumnScope.MessageImage(
     showActionMenu: () -> Unit,
     onSave: () -> Unit
 ) {
-    val image = element.thumbnail.collectAsState().value
-    val thumbnailWidth = element.thumbnailWidth ?: element.width
-    val thumbnailHeight = element.thumbnailHeight ?: element.height
-    val thumbnailLoading = element.thumbnailLoading.collectAsState().value
-    val bitmap = remember(image) {
-        image?.toImageBitmap()
-            ?: if (thumbnailWidth != null && thumbnailHeight != null && thumbnailLoading) {
-                ImageBitmap(thumbnailWidth, thumbnailHeight)
-            } else null
-    }
-    bitmap?.let {
-        MessageImageImpl(it, showActionMenu, onSave, thumbnailLoading)
-    } ?: MessageImageFallback(element, showActionMenu, onSave)
-}
-
-@Composable
-internal fun ColumnScope.MessageImageImpl(
-    image: ImageBitmap,
-    showActionMenu: () -> Unit,
-    onSave: () -> Unit,
-    thumbnailLoading: Boolean
-) {
-    Box {
-        Image(
-            image,
-            "",
-            Modifier
-                .align(Alignment.Center)
-                .padding(3.dp)
-                .heightIn(
-                    50.dp,
-                    with(LocalDensity.current) { 300.dp })
-                .clip(
-                    RoundedCornerShape(8.dp)
-                )
-                .pointerInput(Unit) {
-                    detectTapGestures(
-                        onTap = {
-                            onSave()
-                        },
-                        onLongPress = { showActionMenu() },
-                    )
-                }
-                .buttonPointerModifier(),
-            contentScale = ContentScale.Fit,
-        )
-        if (thumbnailLoading) {
-            LoadingSpinner(Modifier.align(Alignment.Center))
-        }
-    }
-}
-
-@Composable
-internal fun ColumnScope.MessageImageFallback(
-    element: Image,
-    showActionMenu: () -> Unit,
-    onSave: () -> Unit,
-) {
     val i18n = DI.get<I18nView>()
-    Column(
+
+    val thumbnail = rememberImagePainter(element)
+    val fallback = rememberFallbackPainter(element)
+    val imagePainter = animateImage(thumbnail, fallback)
+    val thumbnailLoading = element.thumbnailLoading.collectAsState().value
+
+    val aspectRatioModifier = element.width?.let { width ->
+        element.height?.let { height ->
+            Modifier.aspectRatio(width.toFloat() / height.toFloat())
+        }
+    } ?: Modifier
+
+    Box(
         modifier = Modifier
-            .padding(10.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
+            .padding(3.dp)
+            .requiredHeightIn(50.dp, 300.dp)
+            .then(aspectRatioModifier)
+            .clip(RoundedCornerShape(8.dp))
+            .customClickable(
+                onLongClickLabel = i18n.commonContextMenu(),
+                onLongClick = { showActionMenu() },
+                onClickLabel = i18n.downloadMessage(),
+                onClick = { onSave() }
+            ),
+        contentAlignment = Alignment.Center,
     ) {
-        Icon(
-            MaterialTheme.messengerIcons.typeImage,
-            i18n.commonImage(),
-            Modifier
-                .pointerInput(Unit) {
-                    detectTapGestures(
-                        onTap = {
-                            onSave()
-                        },
-                        onLongPress = { showActionMenu() },
+        if (imagePainter != null) {
+            Image(
+                imagePainter,
+                null,
+                contentScale = ContentScale.Fit,
+                alignment = Alignment.Center,
+                modifier = Modifier.fillMaxSize(),
+            )
+            if (thumbnailLoading) {
+                ThemedSurface(style = MaterialTheme.components.background) {
+                    ThemedProgressIndicator(
+                        style = MaterialTheme.components.circularProgressIndicator,
+                        modifier = Modifier.padding(4.dp),
                     )
                 }
-                .size(64.dp)
-                .buttonPointerModifier()
-        )
-        FileName(element.name)
+            }
+        } else {
+            Column(Modifier.width(48.dp)) {
+                Icon(
+                    Icons.Default.Image,
+                    contentDescription = null,
+                    modifier = Modifier.size(48.dp),
+                )
+                if (thumbnailLoading) {
+                    Spacer(Modifier.height(8.dp))
+                    ThemedProgressIndicator(style = MaterialTheme.components.linearProgressIndicator)
+                }
+            }
+        }
     }
 }
 
@@ -218,22 +207,16 @@ internal fun ImageReplyElement(holder: TimelineElementHolderViewModel, element: 
     ReferencedMessagePill(
         holder = holder,
         content = {
-            val image = element.thumbnail.collectAsState().value
-            val thumbnailWidth = element.thumbnailWidth ?: element.width
-            val thumbnailHeight = element.thumbnailHeight ?: element.height
+            val thumbnail = rememberImagePainter(element)
+            val fallback = rememberFallbackPainter(element)
+            val imagePainter = animateImage(thumbnail, fallback)
             val thumbnailLoading = element.thumbnailLoading.collectAsState().value
-            val bitmap = remember(image) {
-                image?.toImageBitmap()
-                    ?: if (thumbnailWidth != null && thumbnailHeight != null && thumbnailLoading) {
-                        ImageBitmap(thumbnailWidth, thumbnailHeight)
-                    } else null
-            }
 
-            if (bitmap != null) {
+            if (imagePainter != null) {
                 Box {
                     Image(
-                        bitmap,
-                        "",
+                        imagePainter,
+                        null,
                         Modifier.heightIn(max = 100.dp).clip(RoundedCornerShape(8.dp)).align(Alignment.Center),
                         contentScale = ContentScale.Fit
                     )
@@ -253,4 +236,31 @@ internal fun ImageReplyElement(holder: TimelineElementHolderViewModel, element: 
             }
         }
     )
+}
+
+@OptIn(MSC2448::class)
+@Composable
+private fun rememberFallbackPainter(element: Image): Painter? {
+    val thumbnailSize = element.thumbnailWidth?.let { width ->
+        element.thumbnailHeight?.let { height ->
+            IntSize(width, height)
+        }
+    } ?: element.width?.let { width ->
+        element.height?.let { height ->
+            IntSize(width, height)
+        }
+    }
+    return remember(element.blurhash, thumbnailSize) {
+        BlurHashDecoder.decode(element.blurhash, thumbnailSize)
+            ?.let { BitmapPainter(it) }
+    }
+}
+
+@OptIn(MSC2448::class)
+@Composable
+private fun rememberImagePainter(element: Image): Painter? {
+    val thumbnail = element.thumbnail.collectAsState().value
+    return remember(thumbnail) {
+        thumbnail?.toImageBitmap()?.let { BitmapPainter(it) }
+    }
 }
