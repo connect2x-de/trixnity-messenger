2faff73285bf8b2abd1714eac5f049b286b47283
adambrangenberg <adabran06@gmail.com>
2025-08-21 13:45:52 +0200

    Fix clipboard on web

--- trixnity-messenger-compose-view/src/webMain/kotlin/de/connect2x/messenger/compose/view/util/RoomMessageTimelineElementViewModel.web.kt
+++ trixnity-messenger-compose-view/src/webMain/kotlin/de/connect2x/messenger/compose/view/util/RoomMessageTimelineElementViewModel.web.kt
@@ -6,50 +6,38 @@ import androidx.compose.ui.platform.ClipEntry
 import androidx.compose.ui.platform.ClipboardItem
 import de.connect2x.trixnity.messenger.viewmodel.room.timeline.elements.message.RoomMessageTimelineElementViewModel
 import de.connect2x.trixnity.messenger.viewmodel.room.timeline.elements.message.RoomMessageTimelineElementViewModel.*
-import io.ktor.http.ContentType
-import org.w3c.files.Blob
-import web.errors.DOMException
-import web.errors.DOMException.Companion.NotFoundError
-import kotlin.js.Promise
+import kotlinx.browser.window
 
-private fun <T : Any> T.asBlob() = Blob(arrayOf(this))
+@OptIn(ExperimentalComposeUiApi::class)
+fun ClipEntry.Companion.withFormattedText(htmlText: String, plainText: String): ClipEntry {
+    if (window.asDynamic().isSecureContext == false) {
+        println("ClipboardItem is not available in insecure contexts.")
+        return ClipEntry(emptyArray())
+    }
+
+    return ClipEntry(createClipboardItemWithFormattedText(htmlText, plainText))
+}
+
+@Suppress("UNUSED_PARAMETER")
+@OptIn(ExperimentalComposeUiApi::class)
+private fun createClipboardItemWithFormattedText(htmlText: String, plainText: String): Array<ClipboardItem> =
+    js("[new ClipboardItem({'text/plain': new Blob([plainText], { type: 'text/plain' }), 'text/html': new Blob([htmlText], { type: 'text/html' }),})]")
 
 @OptIn(ExperimentalComposeUiApi::class)
 @Composable
-actual fun RoomMessageTimelineElementViewModel<*>.toClipEntry(): ClipEntry? {
-    val items = when (this) {
+actual fun RoomMessageTimelineElementViewModel<*>.toClipEntry(): ClipEntry? =
+    when (this) {
         is TextBased<*> ->
             this.formattedBody?.let {
-                mapOf(
-                    ContentType.Text.Html to it,
-                    ContentType.Text.Plain to this.body
-                )
-            } ?: mapOf(
-                ContentType.Text.Plain to this.body
-            )
+                ClipEntry.withFormattedText(it, this.body)
+            } ?: ClipEntry.withPlainText(this.body)
 
+        // FIXME should deliver caption
+        is FileBased<*> -> ClipEntry(emptyArray())
 
-        is FileBased<*> -> mapOf<Any, Any>() // FIXME should deliver caption
-
-        is Location -> mapOf(
-            ContentType.Text.Plain to this.coordinates // FIXME should deliver proper location description (placename, coordinates)
-        )
+        // FIXME should deliver proper location description (placename, coordinates)
+        is Location -> ClipEntry.withPlainText(this.coordinates)
 
         is VerificationRequest,
-        is Unknown -> mapOf<Any, Any>()
-    }.mapKeys { it.toString() }.mapValues { it.asBlob() }
-
-    return if (items.isEmpty()) null else ClipEntry(
-        arrayOf(
-            object : ClipboardItem {
-                override val types: Array<String> = items.keys.toTypedArray()
-
-                override fun getType(type: String): Promise<Blob> {
-                    return items[type]?.let {
-                        Promise.resolve(it)
-                    } ?: Promise.reject(DOMException("No representation of $type found", NotFoundError))
-                }
-            }
-        )
-    )
-}
+        is Unknown -> ClipEntry(emptyArray())
+    }.takeIf { it.clipboardItems.isNotEmpty() }
