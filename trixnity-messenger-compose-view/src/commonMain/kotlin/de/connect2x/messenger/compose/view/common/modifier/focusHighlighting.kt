f4701d21da9626339e0accdce7707bad0020e4ed
michael.thiele <thiele.michael2@gmail.com>
2025-08-26 13:41:59 +0200

    possible perf fix

--- trixnity-messenger-compose-view/src/commonMain/kotlin/de/connect2x/messenger/compose/view/common/modifier/focusHighlighting.kt
+++ trixnity-messenger-compose-view/src/commonMain/kotlin/de/connect2x/messenger/compose/view/common/modifier/focusHighlighting.kt
@@ -5,8 +5,10 @@ import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.interaction.collectIsFocusedAsState
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Stable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Dp
 import de.connect2x.messenger.compose.view.theme.IsFocusHighlighting
 import de.connect2x.messenger.compose.view.theme.messengerFocusIndicator
 
@@ -17,11 +19,26 @@ fun Modifier.focusHighlighting(
 ): Modifier {
     val hasFocus = interactionSource.collectIsFocusedAsState().value
 
+    return this then focusHighlighting(
+        hasFocus,
+        IsFocusHighlighting.current,
+        MaterialTheme.messengerFocusIndicator.borderWidth,
+        color,
+    )
+}
+
+@Stable
+fun Modifier.focusHighlighting(
+    hasFocus: Boolean,
+    isFocusHighlightingActive: Boolean,
+    borderWidth: Dp,
+    color: Color,
+): Modifier {
     return this
         .then(
-            if (IsFocusHighlighting.current && hasFocus) {
+            if (isFocusHighlightingActive && hasFocus) {
                 Modifier.border(
-                    width = MaterialTheme.messengerFocusIndicator.borderWidth,
+                    width = borderWidth,
                     color = color,
                 )
             } else Modifier
