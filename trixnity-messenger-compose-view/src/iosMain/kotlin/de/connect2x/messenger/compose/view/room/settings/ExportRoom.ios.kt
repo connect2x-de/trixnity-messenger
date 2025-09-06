513f7398b9d6a853941a3c6b092fea5de76975e5
michael.thiele <thiele.michael2@gmail.com>
2025-09-06 09:56:39 +0200

    a bit more debugging

--- trixnity-messenger-compose-view/src/iosMain/kotlin/de/connect2x/messenger/compose/view/room/settings/ExportRoom.ios.kt
+++ trixnity-messenger-compose-view/src/iosMain/kotlin/de/connect2x/messenger/compose/view/room/settings/ExportRoom.ios.kt
@@ -5,7 +5,7 @@ import de.connect2x.trixnity.messenger.export.Destination
 import de.connect2x.trixnity.messenger.export.FileBasedExportRoomProperties
 import io.github.oshai.kotlinlogging.KotlinLogging
 import okio.Path.Companion.toPath
-import platform.Foundation.NSDocumentDirectory
+import platform.Foundation.NSDownloadsDirectory
 import platform.Foundation.NSSearchPathForDirectoriesInDomains
 import platform.Foundation.NSUserDomainMask
 
@@ -17,9 +17,11 @@ internal actual fun SelectExportDestination(
     result: (Destination?) -> Unit
 ) {
     // TODO this does save it in the app bundle and not locally on the phone so the user has access to it!
-    val documentsPath = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, true)
+    // TODO NSLocalDomainMask should return a system directory, but this is null
+    val documentsPath = NSSearchPathForDirectoriesInDomains(NSDownloadsDirectory, NSUserDomainMask, true)
         .firstOrNull() as? String
     if (documentsPath == null) {
+        log.debug { "cannot export, as directory is not accessible " }
         result(null)
         return
     }
