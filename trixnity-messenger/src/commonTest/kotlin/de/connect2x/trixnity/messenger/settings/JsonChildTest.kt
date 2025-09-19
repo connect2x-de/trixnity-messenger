adb3709a81fddf66bf9c4821d78f0bac7055f895
benkuly <kontakt@folivo.net>
2025-09-19 14:21:02 +0200

    Technical: Don't override siblings or children in the deep nested settings view

--- trixnity-messenger/src/commonTest/kotlin/de/connect2x/trixnity/messenger/settings/JsonChildTest.kt
+++ trixnity-messenger/src/commonTest/kotlin/de/connect2x/trixnity/messenger/settings/JsonChildTest.kt
@@ -56,12 +56,25 @@ class JsonChildTest {
     }
 
     @Test
-    fun `putJsonChild - empty keys - put into root`() {
+    fun `putJsonChild - empty keys - override into root`() {
         putJsonChild(
-            JsonObject(mapOf("dino" to JsonObject(mapOf("a" to JsonPrimitive("unicorn"))))),
+            JsonObject(
+                mapOf(
+                    "dino" to JsonObject(mapOf("a" to JsonPrimitive("unicorn"))),
+                    "c" to JsonPrimitive("value")
+                )
+            ),
             JsonObject(mapOf("dino" to JsonObject(mapOf("b" to JsonPrimitive("unicorn"))))),
-        ) shouldBe JsonObject(mapOf("dino" to JsonObject(mapOf("b" to JsonPrimitive("unicorn")))))
-
+        ) shouldBe JsonObject(
+            mapOf(
+                "dino" to JsonObject(
+                    mapOf(
+                        "b" to JsonPrimitive("unicorn")
+                    )
+                ),
+                "c" to JsonPrimitive("value"),
+            )
+        )
     }
 
     @Test
