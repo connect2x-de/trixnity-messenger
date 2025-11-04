06c96ed31c03d769c79d229efa22023b8b675e32
Merlin Volkmer <merlin.volkmer@outlook.com>
2025-11-04 10:19:26 +0100

    make side panes grab focus when opened

---
+++ trixnity-messenger-compose-view/src/commonMain/kotlin/de/connect2x/messenger/compose/view/common/modifier/focusOnFirstRender.kt
@@ -0,0 +1,26 @@
+package de.connect2x.messenger.compose.view.common.modifier
+
+import androidx.compose.foundation.focusable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+
+fun Modifier.focusOnFirstRender(): Modifier = composed {
+    val focusRequester = remember { FocusRequester() }
+    var hasRequested by remember { mutableStateOf(false) }
+
+    LaunchedEffect(Unit) {
+        if (!hasRequested) {
+            focusRequester.requestFocus()
+            hasRequested = true
+        }
+    }
+
+    this.focusRequester(focusRequester).focusable()
+}
