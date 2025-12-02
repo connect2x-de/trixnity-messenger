d64f22ef9c8d1715320159bcd8d1d3ac2f6e296f
michael.thiele <thiele.michael2@gmail.com>
2025-12-02 16:46:42 +0100

    add vertical divider after room list (for 2- or 3-sided layout)

--- trixnity-messenger-compose-view/src/commonMain/kotlin/de/connect2x/messenger/compose/view/roomlist/RoomListSwitch.kt
+++ trixnity-messenger-compose-view/src/commonMain/kotlin/de/connect2x/messenger/compose/view/roomlist/RoomListSwitch.kt
@@ -1,7 +1,12 @@
 package de.connect2x.messenger.compose.view.roomlist
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.arkivanov.decompose.extensions.compose.stack.Children
 import com.arkivanov.decompose.extensions.compose.stack.animation.fade
 import com.arkivanov.decompose.extensions.compose.stack.animation.stackAnimation
@@ -17,6 +22,7 @@ import de.connect2x.messenger.compose.view.settings.NotificationsSettings
 import de.connect2x.messenger.compose.view.settings.PrivacySettings
 import de.connect2x.messenger.compose.view.settings.ProfileSettings
 import de.connect2x.messenger.compose.view.settings.UserSettings
+import de.connect2x.messenger.compose.view.theme.components.ThemedVerticalDivider
 import de.connect2x.trixnity.messenger.viewmodel.MainViewModel
 import de.connect2x.trixnity.messenger.viewmodel.roomlist.RoomListRouter
 
@@ -28,21 +34,29 @@ fun RoomListSwitch(
         stack = mainViewModel.roomListRouterStack,
         animation = stackAnimation(fade())
     ) {
-        when (val child = it.instance) {
-            is RoomListRouter.Wrapper.List -> RoomListContainer(child.viewModel)
-            is RoomListRouter.Wrapper.CreateNewChat -> CreateNewChat(child.viewModel)
-            is RoomListRouter.Wrapper.CreateNewGroup -> CreateNewGroup(child.viewModel)
-            is RoomListRouter.Wrapper.SearchGroup -> SearchGroup(child.viewModel)
-            is RoomListRouter.Wrapper.UserSettings -> UserSettings(child.viewModel, mainViewModel)
-            is RoomListRouter.Wrapper.DevicesSettings -> DevicesSettings(child.viewModel)
-            is RoomListRouter.Wrapper.Profile -> ProfileSettings(child.viewModel)
-            is RoomListRouter.Wrapper.NotificationsSettings -> NotificationsSettings(child.viewModel)
-            is RoomListRouter.Wrapper.PrivacySettings -> PrivacySettings(child.viewModel)
-            is RoomListRouter.Wrapper.AppearanceSettings -> AppearanceSettings(child.viewModel)
-            is RoomListRouter.Wrapper.BlockedContactsSettings -> BlockedContactsSettings(child.viewModel)
-            is RoomListRouter.Wrapper.AppInfo -> AppInfo(child.viewModel)
-            is RoomListRouter.Wrapper.AccountsOverview -> AccountsOverview(child.viewModel)
-            is RoomListRouter.Wrapper.None -> Box {}
-        }.let {}
+        Box {
+            when (val child = it.instance) {
+                is RoomListRouter.Wrapper.List -> RoomListContainer(child.viewModel)
+                is RoomListRouter.Wrapper.CreateNewChat -> CreateNewChat(child.viewModel)
+                is RoomListRouter.Wrapper.CreateNewGroup -> CreateNewGroup(child.viewModel)
+                is RoomListRouter.Wrapper.SearchGroup -> SearchGroup(child.viewModel)
+                is RoomListRouter.Wrapper.UserSettings -> UserSettings(child.viewModel, mainViewModel)
+                is RoomListRouter.Wrapper.DevicesSettings -> DevicesSettings(child.viewModel)
+                is RoomListRouter.Wrapper.Profile -> ProfileSettings(child.viewModel)
+                is RoomListRouter.Wrapper.NotificationsSettings -> NotificationsSettings(child.viewModel)
+                is RoomListRouter.Wrapper.PrivacySettings -> PrivacySettings(child.viewModel)
+                is RoomListRouter.Wrapper.AppearanceSettings -> AppearanceSettings(child.viewModel)
+                is RoomListRouter.Wrapper.BlockedContactsSettings -> BlockedContactsSettings(child.viewModel)
+                is RoomListRouter.Wrapper.AppInfo -> AppInfo(child.viewModel)
+                is RoomListRouter.Wrapper.AccountsOverview -> AccountsOverview(child.viewModel)
+                is RoomListRouter.Wrapper.None -> Box {}
+            }.let {}
+            ThemedVerticalDivider(
+                Modifier
+                    .fillMaxHeight()
+                    .width(1.dp)
+                    .align(Alignment.CenterEnd)
+            )
+        }
     }
 }
