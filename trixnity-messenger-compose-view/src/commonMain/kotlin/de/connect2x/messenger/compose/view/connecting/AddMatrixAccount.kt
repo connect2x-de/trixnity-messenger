cae678894145d76a00bec06f7c2a82543e25bfe6
jlddorf <jakob.deutschendorf@gmail.com>
2026-01-09 22:20:41 +0100

    change styling for warning

--- trixnity-messenger-compose-view/src/commonMain/kotlin/de/connect2x/messenger/compose/view/connecting/AddMatrixAccount.kt
+++ trixnity-messenger-compose-view/src/commonMain/kotlin/de/connect2x/messenger/compose/view/connecting/AddMatrixAccount.kt
@@ -1,17 +1,29 @@
 package de.connect2x.messenger.compose.view.connecting
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Warning
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import de.connect2x.messenger.compose.view.DI
+import de.connect2x.messenger.compose.view.common.MiddleSpacer
+import de.connect2x.messenger.compose.view.common.SmallSpacer
 import de.connect2x.messenger.compose.view.get
 import de.connect2x.messenger.compose.view.i18n.I18nView
+import de.connect2x.messenger.compose.view.theme.components
+import de.connect2x.messenger.compose.view.theme.messengerDpConstants
+import de.connect2x.messenger.compose.view.theme.messengerIcons
 import de.connect2x.trixnity.messenger.viewmodel.connecting.AddMatrixAccountState
 import de.connect2x.trixnity.messenger.viewmodel.connecting.AddMatrixAccountViewModel
 
@@ -33,11 +45,19 @@ class AddMatrixAccountViewImpl : AddMatrixAccountView {
             ServerInputField(addMatrixAccountViewModel)
             Spacer(Modifier.height(20.dp))
             ServerDiscoveryState(addMatrixAccountViewModel)
-            Surface {
+            MiddleSpacer()
+            Surface(color = MaterialTheme.colorScheme.errorContainer, shape = MaterialTheme.shapes.medium) {
                 val isMultiProfile = addMatrixAccountViewModel.isMultiProfile.collectAsState().value
-                Column {
-                    Text(i18n.accountOverviewWarning())
-                    if (isMultiProfile) Text(i18n.accountOverviewWarningMultipleAccounts())
+                Row(
+                    Modifier.padding(MaterialTheme.messengerDpConstants.middle),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Icon(Icons.Default.Warning, "Warning")
+                    SmallSpacer()
+                    Column {
+                        Text(i18n.accountOverviewWarning())
+                        if (isMultiProfile) Text(i18n.accountOverviewWarningMultipleAccounts())
+                    }
                 }
             }
         }
