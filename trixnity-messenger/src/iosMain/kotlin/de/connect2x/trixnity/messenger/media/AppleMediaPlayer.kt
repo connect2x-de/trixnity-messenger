29a1f1bbc5c05577e10473b3d760a1ba50210837
cach30verfl0w <contact@cach30verfl0w.net>
2026-02-04 18:49:12 +0100

    fix: Don't run withPlayer closure on iOS when player initalization fails

--- trixnity-messenger/src/iosMain/kotlin/de/connect2x/trixnity/messenger/media/AppleMediaPlayer.kt
+++ trixnity-messenger/src/iosMain/kotlin/de/connect2x/trixnity/messenger/media/AppleMediaPlayer.kt
@@ -77,6 +77,7 @@ internal class AppleMediaPlayer : MediaPlayer {
             } catch (ex: Exception) {
                 log.error(ex) { "Unable to prepare media player" }
                 state.value = MediaPlayer.State.Failed("Unable to prepare media player: ${ex.message}")
+                return
             }
         }
 
