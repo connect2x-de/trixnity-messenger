c3a318afb44af4a24ada34b4a05bfe3392301cb4
Alexander Hinze <alexh@karmakrafts.dev>
2026-02-11 16:26:51 +0000

    Fix logging on iOS and Windows

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -25,7 +25,7 @@ dependencyResolutionManagement {
 }
 
 plugins {
-    id("de.connect2x.conventions.c2x-settings-plugin") version "20260129.102940"
+    id("de.connect2x.conventions.c2x-settings-plugin") version "20260211.153643"
 }
 
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
