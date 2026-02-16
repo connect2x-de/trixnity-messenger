92a7ba021b4e6fd1a21abcf736d78c1e775508cd
michael.thiele <thiele.michael2@gmail.com>
2026-02-16 05:33:33 +0100

    add issue link in code

--- trixnity-messenger-compose-view/src/jvmMain/kotlin/de/connect2x/trixnity/messenger/compose/view/theme/ThemeSettings.desktop.kt
+++ trixnity-messenger-compose-view/src/jvmMain/kotlin/de/connect2x/trixnity/messenger/compose/view/theme/ThemeSettings.desktop.kt
@@ -7,6 +7,7 @@ import org.jetbrains.skiko.SystemTheme
 import org.jetbrains.skiko.currentSystemTheme
 
 // since Compose itself does not update the value on its own, use this neat trick
+// see https://youtrack.jetbrains.com/issue/CMP-1986/isSystemInDarkTheme-should-dynamically-update-when-system-theme-is-changed#focus=Comments-27-12665870.0-0
 @Composable
 actual fun isDarkTheme(): Boolean {
     return produceState(initialValue = currentSystemTheme == SystemTheme.DARK) {
