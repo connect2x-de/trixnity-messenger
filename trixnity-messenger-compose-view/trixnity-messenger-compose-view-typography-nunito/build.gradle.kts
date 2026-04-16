04ea83b0302821046dddfcc8877224e85ccd7711
Matej Minar <matejminar02@gmail.com>
2026-04-16 19:34:10 +0200

    Fixed nunito fonts not working on android

--- trixnity-messenger-compose-view/trixnity-messenger-compose-view-typography-nunito/build.gradle.kts
+++ trixnity-messenger-compose-view/trixnity-messenger-compose-view-typography-nunito/build.gradle.kts
@@ -2,6 +2,7 @@
 
 import de.connect2x.conventions.configureJava
 import de.connect2x.conventions.defaultCompilerOptions
+import de.connect2x.conventions.withAndroid
 import de.connect2x.conventions.withBrowser
 import de.connect2x.conventions.withIos
 import de.connect2x.conventions.withJvm
@@ -11,6 +12,7 @@ import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 
 plugins {
     alias(sharedLibs.plugins.kotlin.multiplatform)
+    alias(sharedLibs.plugins.android.library)
     alias(sharedLibs.plugins.compose.multiplatform)
     alias(sharedLibs.plugins.compose.compiler)
 }
@@ -25,6 +27,7 @@ kotlin {
         withBrowser()
     }
     withIos()
+    withAndroid("$group.compose.app", minSdk = libs.versions.minSdkVersion)
 
     sourceSets {
         commonMain.dependencies {
