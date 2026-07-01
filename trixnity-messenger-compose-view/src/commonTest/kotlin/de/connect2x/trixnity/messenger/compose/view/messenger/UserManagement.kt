748bc4b7dc776330289dd7a2e84d7fc73d679fbe
Felix Hilgers <contact@fhilgers.com>
2026-07-01 17:55:16 +0200

    chore: ktfmtFormat

--- trixnity-messenger-compose-view/src/commonTest/kotlin/de/connect2x/trixnity/messenger/compose/view/messenger/UserManagement.kt
+++ trixnity-messenger-compose-view/src/commonTest/kotlin/de/connect2x/trixnity/messenger/compose/view/messenger/UserManagement.kt
@@ -4,6 +4,5 @@ import de.connect2x.trixnity.messenger.compose.view.util.SynapseAdmin
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
-suspend fun createUser(username: String, password: String) = withContext(Dispatchers.Default) {
-    SynapseAdmin.registerNewUser(username, password)
-}
+suspend fun createUser(username: String, password: String) =
+    withContext(Dispatchers.Default) { SynapseAdmin.registerNewUser(username, password) }
