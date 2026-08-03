226f6e455364cdbf0456e1c866cbb7e0ff920ff0
Felix Hilgers <contact@fhilgers.com>
2026-08-03 15:11:46 +0200

    feat: add RouteNavigation helpers

--- trixnity-messenger/src/commonMain/kotlin/de/connect2x/trixnity/messenger/internal/navigation/RouteNavigationExt.kt
+++ trixnity-messenger/src/commonMain/kotlin/de/connect2x/trixnity/messenger/internal/navigation/RouteNavigationExt.kt
@@ -3,7 +3,14 @@
 package de.connect2x.trixnity.messenger.internal.navigation
 
 import com.arkivanov.decompose.ExperimentalDecomposeApi
+import de.connect2x.trixnity.core.model.RoomId
+import de.connect2x.trixnity.core.model.UserId
 import de.connect2x.trixnity.messenger.abi.TrixnityMessengerPrivateApi
+import de.connect2x.trixnity.messenger.internal.routes.TimelineRoute
+import de.connect2x.trixnity.messenger.internal.routes.extras.ExtrasRouteMarker
+import de.connect2x.trixnity.messenger.internal.routes.roomlist.RoomListRoute
+import de.connect2x.trixnity.messenger.internal.routes.roomlist.RoomListRouteMarker
+import de.connect2x.trixnity.messenger.internal.routes.root.RootRouteMarker
 
 @TrixnityMessengerPrivateApi
 fun RouteNavigation.navigationCallback(transform: RouteNavigationScope.() -> Unit): () -> Unit = {
@@ -25,3 +32,21 @@ fun <T1, T2> RouteNavigation.navigationCallback(transform: RouteNavigationScope.
 fun <T1, T2, T3> RouteNavigation.navigationCallback(
     transform: RouteNavigationScope.(T1, T2, T3) -> Unit
 ): (T1, T2, T3) -> Unit = { t1, t2, t3 -> updateNavigation { transform(t1, t2, t3) } }
+
+@TrixnityMessengerPrivateApi
+fun RouteNavigationScope.login() {
+    clear<RootRouteMarker>()
+    replace<RoomListRouteMarker>(RoomListRoute)
+}
+
+@TrixnityMessengerPrivateApi
+fun RouteNavigationScope.openRoom(userId: UserId, roomId: RoomId) {
+    closeRoom()
+    push<TimelineRoute>(TimelineRoute(userId, roomId))
+}
+
+@TrixnityMessengerPrivateApi
+fun RouteNavigationScope.closeRoom() {
+    clear<TimelineRoute>()
+    clear<ExtrasRouteMarker>()
+}
