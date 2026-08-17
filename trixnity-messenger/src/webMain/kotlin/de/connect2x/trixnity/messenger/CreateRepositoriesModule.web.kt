7c0642af07d392f16d4bdf78268f53bc76e9ee35
Felix Hilgers <contact@fhilgers.com>
2026-08-17 04:45:17 +0200

    refactor: split out IndexedDBCreateRepositoriesModule

--- trixnity-messenger/src/webMain/kotlin/de/connect2x/trixnity/messenger/CreateRepositoriesModule.web.kt
+++ trixnity-messenger/src/webMain/kotlin/de/connect2x/trixnity/messenger/CreateRepositoriesModule.web.kt
@@ -1,30 +1,9 @@
 package de.connect2x.trixnity.messenger
 
-import de.connect2x.trixnity.client.RepositoriesModule
-import de.connect2x.trixnity.client.store.repository.indexeddb.indexedDB
-import de.connect2x.trixnity.core.model.UserId
 import de.connect2x.trixnity.messenger.util.RootPath
 import org.koin.core.module.Module
 import org.koin.dsl.module
 
 actual fun platformCreateRepositoriesModuleModule(): Module = module {
-    single<CreateRepositoriesModule> {
-        val rootPath = get<RootPath>()
-        object : CreateRepositoriesModule {
-            override suspend fun generateDatabaseKey(): ByteArray? = null
-
-            override suspend fun create(userId: UserId, databaseKey: ByteArray?): RepositoriesModule =
-                createInternal(userId)
-
-            override suspend fun load(userId: UserId, databaseKey: ByteArray?): RepositoriesModule =
-                createInternal(userId)
-
-            override fun handleExceptions(exc: Exception) {
-                // empty
-            }
-
-            private fun createInternal(userId: UserId): RepositoriesModule =
-                RepositoriesModule.indexedDB(rootPath.forAccountDatabase(userId).toString())
-        }
-    }
+    single<CreateRepositoriesModule> { IndexedDBCreateRepositoriesModule(rootPath = get<RootPath>()) }
 }
